e894eb7971e849f00ff851ab797393db4892c589
timtay-microsoft <timtay@microsoft.com>
2024-08-30 16:57:03 -0700

    fix pathing?

--- tools/deployment/deploy-aio.sh
+++ tools/deployment/deploy-aio.sh
@@ -42,8 +42,7 @@ rm -rf ~/.step
 helm upgrade trust-manager jetstack/trust-manager --install --create-namespace -n azure-iot-operations --set app.trust.namespace=azure-iot-operations --wait
 
 # install cert issuers and trust bundle
-dir
-kubectl apply -f ./yaml/certificates.yaml
+kubectl apply -f ../../.devcontainer/yaml/certificates.yaml
 
 # Wait for CA trust bundle to be generated for external connections to the MQTT Broker and then add to the local env
 while ! kubectl get secret aio-mq-broker-external-ca -n azure-iot-operations; do
