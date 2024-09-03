94bd3c9453e430c39f21f3bfa410e675ae907f54
timtay-microsoft <timtay@microsoft.com>
2024-09-03 11:32:52 -0700

    newlines?

--- dotnet/samples/TestEnvoys/gen.sh
+++ dotnet/samples/TestEnvoys/gen.sh
@@ -2,4 +2,3 @@
 ../../../codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile math.json --outDir . --sdkPath ../../src/Azure.Iot.Operations.Protocol
 ../../../codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile memmon.json --outDir . --sdkPath ../../src/Azure.Iot.Operations.Protocol
 ../../../codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile passthrough.json --outDir . --sdkPath ../../src/Azure.Iot.Operations.Protocol
-
