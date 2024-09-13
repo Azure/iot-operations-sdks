b1fbf537081a03e67f51e36a63f644b66e491e99
Carter Tinney <carter.tinney@microsoft.com>
2024-09-13 09:28:05 -0700

    added resources

--- rust/azure_iot_operations_protocol/examples/counter_client.rs
+++ rust/azure_iot_operations_protocol/examples/counter_client.rs
@@ -21,7 +21,7 @@ const REQUEST_TOPIC_PATTERN: &str = "rpc/command-samples/{executorId}/{commandNa
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     Builder::new()
-        .filter_level(log::LevelFilter::Warn)
+        .filter_level(log::LevelFilter::max())
         .format_timestamp(None)
         .filter_module("rumqttc", log::LevelFilter::Warn)
         .init();
