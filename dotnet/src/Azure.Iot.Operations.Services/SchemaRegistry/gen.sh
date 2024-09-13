ca9dcb26b91b14f90ad00a51b3f2d63052f188fc
timtay-microsoft <timtay@microsoft.com>
2024-09-13 12:08:30 -0700

    Fix code gen pathing in state store + schema registry scripts

--- dotnet/src/Azure.Iot.Operations.Services/SchemaRegistry/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/SchemaRegistry/gen.sh
@@ -1,3 +1,3 @@
-../../../../../tools/codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile SchemaRegistry-1.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry
+../../../../codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile SchemaRegistry-1.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry
 cp -f /tmp/Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry/dtmi_ms_adr_SchemaRegistry__1/*.cs dtmi_ms_adr_SchemaRegistry__1 -v
 rm -rf /tmp/Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry -v
