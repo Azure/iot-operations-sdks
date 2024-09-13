6ee9d7db2c60fd16a45845f773f9f4fd5bdb0f9d
timtay-microsoft <timtay@microsoft.com>
2024-09-13 14:50:54 -0700

    maybe

--- dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
@@ -1,6 +1,3 @@
-../../../../codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile dss.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.StateStore.Gen
 rm -rf ./StateStoreGen
 mkdir ./StateStoreGen
-cp -f /tmp/Azure.Iot.Operations.Services.StateStore.Gen/dtmi_ms_aio_mq_StateStore__1/*.cs ./StateStoreGen -v
-cp -f /tmp/Azure.Iot.Operations.Services.StateStore.Gen/PassthroughSerializer.cs ./StateStoreGen -v
-rm -rf /tmp/Azure.Iot.Operations.Services.StateStore.Gen -v
+../../../../codegen/src/Akri.Dtdl.Codegen/bin/Debug/net8.0/Akri.Dtdl.Codegen --modelFile dss.json --lang csharp --outDir ./StateStoreGen
