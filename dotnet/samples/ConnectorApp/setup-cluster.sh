d8feacce1757b30e81777e607b3ea239aacaa870
Tim Taylor <timtay@microsoft.com>
2024-10-03 19:14:39 +0000

    asdf

--- dotnet/samples/ConnectorApp/setup-cluster.sh
+++ dotnet/samples/ConnectorApp/setup-cluster.sh
@@ -1,22 +1,18 @@
 # Create k3d cluster with local image registry
-k3d cluster delete
-k3d cluster create -p '1883:1883@loadbalancer' -p '8883:8883@loadbalancer'
+../../../tools/deployment/initialize-cluster.sh
 
 # Deploy Broker
-helm install broker --atomic oci://mqbuilds.azurecr.io/helm/aio-broker --version 0.7.0-nightly
-kubectl apply -f ./broker.yaml 
-
-# Deploy connector config
-kubectl apply -f ./connector-config.yaml
-
-# Deploy Operator helm chart
-helm install akri-operator oci://akribuilds.azurecr.io/helm/microsoft-managed-akri-operator --version 0.4.0-main-20241003.2-buddy
+../../../tools/deployment/deploy-aio.sh nightly
 
 # Deploy ADR
 helm install adrcommonprp --version 0.3.0 oci://azureadr.azurecr.io/helm/adr/common/adr-crds-prp
 
 # Build connector image
 dotnet publish /t:PublishContainer
+k3d image import http-connector-worker-service:latest -c k3s-default
+
+# Deploy connector config
+kubectl apply -f ./connector-config.yaml
 
 # Build HTTP server docker image
 docker build -t http-server:latest ./SampleHttpServer
@@ -28,3 +24,6 @@ kubectl apply -f ./SampleHttpServer/http-server.yaml
 
 # Deploy HTTP server AEP
 kubectl apply -f ./http-server-aep.yaml
+
+# Deploy Operator helm chart
+helm install akri-operator oci://akribuilds.azurecr.io/helm/microsoft-managed-akri-operator --version 0.4.0-main-20241003.2-buddy
