dfc2a302869a5e3068107bc0b62ab4bd5d129bf7
timtay-microsoft <timtay@microsoft.com>
2024-10-04 16:54:19 -0700

    back to previous namespace

--- dotnet/samples/ConnectorApp/setup-cluster.sh
+++ dotnet/samples/ConnectorApp/setup-cluster.sh
@@ -5,7 +5,7 @@
 ../../../tools/deployment/deploy-aio.sh nightly
 
 # Deploy ADR
-helm install adrcommonprp --version 0.3.0 oci://azureadr.azurecr.io/helm/adr/common/adr-crds-prp -n default --wait
+helm install adrcommonprp --version 0.3.0 oci://azureadr.azurecr.io/helm/adr/common/adr-crds-prp -n azure-iot-operations --wait
 
 # Build connector image
 dotnet publish /t:PublishContainer
@@ -26,10 +26,11 @@ kubectl apply -f ./http-connector-secrets.yaml
 kubectl apply -f ./http-server-aep.yaml
 
 # Deploy Operator helm chart
-helm install akri-operator oci://akribuilds.azurecr.io/helm/microsoft-managed-akri-operator --version 0.4.0-main-20241004.2-buddy -n default --wait
+helm install akri-operator oci://akribuilds.azurecr.io/helm/microsoft-managed-akri-operator --version 0.4.0-main-20241004.2-buddy -n azure-iot-operations --wait
 
 # TODO this should be part of the above helm chart. Sync w/ Abhipsa/Daniel
 kubectl apply -f ./connector_config_crd.yaml
 
 # Deploy connector config
 kubectl apply -f ./connector-config.yaml
+
