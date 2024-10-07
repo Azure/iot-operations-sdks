2cf068ab3cd424786aba5abd3e75583944b218db
timtay-microsoft <timtay@microsoft.com>
2024-10-07 16:03:28 -0700

    no mqtt for now

--- dotnet/samples/ConnectorApp/Worker.cs
+++ dotnet/samples/ConnectorApp/Worker.cs
@@ -70,7 +70,7 @@ namespace DotnetHttpConnectorWorkerService
                 MqttConnectionSettings mqttConnectionSettings = null;
                 MqttSessionClient sessionClient = null;
 
-                await sessionClient.ConnectAsync(mqttConnectionSettings);
+                //await sessionClient.ConnectAsync(mqttConnectionSettings);
 
                 while (true)
                 {
@@ -79,7 +79,7 @@ namespace DotnetHttpConnectorWorkerService
 
                     Console.WriteLine("Read data from http asset endpoint:");
                     Console.WriteLine(httpData + "\n");
-
+                    /*
                     var sender = new StringTelemetrySender(sessionClient)
                     {
                         TopicPattern = "sample",
@@ -90,6 +90,7 @@ namespace DotnetHttpConnectorWorkerService
                     {
                         await sender.SendTelemetryAsync(httpData);
                     }
+                    */
 
                     await Task.Delay(TimeSpan.FromSeconds(5));
                 }
