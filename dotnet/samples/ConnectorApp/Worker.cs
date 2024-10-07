eea6dee7cbfc04c9a70c7fd036ed09f338573e21
timtay-microsoft <timtay@microsoft.com>
2024-10-07 16:45:54 -0700

    Revert "mqtt back in"
    This reverts commit b091edc4b87469781e3c13f7cee1952f41fd0d75.

--- dotnet/samples/ConnectorApp/Worker.cs
+++ dotnet/samples/ConnectorApp/Worker.cs
@@ -71,7 +71,7 @@ namespace DotnetHttpConnectorWorkerService
                 MqttConnectionSettings mqttConnectionSettings = null;
                 MqttSessionClient sessionClient = null;
 
-                await sessionClient.ConnectAsync(mqttConnectionSettings);
+                //await sessionClient.ConnectAsync(mqttConnectionSettings);
 
                 while (true)
                 {
@@ -80,7 +80,7 @@ namespace DotnetHttpConnectorWorkerService
 
                     Console.WriteLine("Read data from http asset endpoint:");
                     Console.WriteLine(httpData + "\n");
-                    
+                    /*
                     var sender = new StringTelemetrySender(sessionClient)
                     {
                         TopicPattern = "sample",
@@ -91,6 +91,7 @@ namespace DotnetHttpConnectorWorkerService
                     {
                         await sender.SendTelemetryAsync(httpData);
                     }
+                    */
 
                     await Task.Delay(samplingInterval);
                 }
