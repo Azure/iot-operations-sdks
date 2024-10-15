22306cfa326b3bf19b09e5b13c0cf0beb08c8cda
Ryan Winter <ryanwinter@outlook.com>
2024-10-15 21:10:11 +0000

    move window processing into new thread
    Signed-off-by: Ryan Winter <ryanwinter@outlook.com>

--- dotnet/tutorials/EventDrivenApp/InputWorker.cs
+++ dotnet/tutorials/EventDrivenApp/InputWorker.cs
@@ -5,19 +5,20 @@ using Azure.Iot.Operations.Mqtt.Session;
 using Azure.Iot.Operations.Services.StateStore;
 using Azure.Iot.Operations.Protocol.Telemetry;
 using System.Text.Json;
+using System.Collections.Concurrent;
 
 namespace EventDrivenApp;
 
 public class InputWorker(SessionClientFactory clientFactory, ILogger<InputWorker> logger) : BackgroundService
 {
-    private MqttSessionClient? sessionClient = null;
+    private readonly BlockingCollection<SensorData> incomingSensorData = [];
 
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
         try
         {
-            // Get a session client
-            sessionClient = await clientFactory.GetSessionClient("input");
+            // Get a connected session client
+            MqttSessionClient sessionClient = await clientFactory.GetSessionClient("input");
 
             // Start the telemetry receiver
             var receiver = new SensorTelemetryReceiver(sessionClient)
@@ -26,8 +27,8 @@ public class InputWorker(SessionClientFactory clientFactory, ILogger<InputWorker
             };
             await receiver.StartAsync(cancellationToken);
 
-            // Wait forever
-            await Task.Delay(Timeout.Infinite, cancellationToken);
+            // Enter main loop to process the sensor data
+            await ProcessSensorData(sessionClient, cancellationToken);
 
             await sessionClient.DisconnectAsync(null, cancellationToken);
             Environment.Exit(0);
@@ -39,45 +40,66 @@ public class InputWorker(SessionClientFactory clientFactory, ILogger<InputWorker
         }
     }
 
-    private async Task ReceiveTelemetry(string senderId, SensorData sensor, IncomingTelemetryMetadata metadata)
+    private Task ReceiveTelemetry(string senderId, SensorData sensor, IncomingTelemetryMetadata metadata)
     {
         logger.LogInformation($"Received sensor data");
 
-        List<SensorData> data = [];
-        await using StateStoreClient stateStoreClient = new(sessionClient!);
+        incomingSensorData.Add(sensor);
+
+        return Task.CompletedTask;
+    }
+
+    private async Task ProcessSensorData(MqttSessionClient sessionClient, CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
         {
-            try
+            SensorData sensor = incomingSensorData.Take(cancellationToken);
+
+            logger.LogInformation("Processing sensor data");
+
+            List<SensorData> data = [];
+
+            await using StateStoreClient stateStoreClient = new(sessionClient);
             {
-                // Fetch the historical sensor data from the state store
-                StateStoreGetResponse response = await stateStoreClient.GetAsync(Constants.StateStoreSensorKey, null);
-                if (response.Value != null)
+                try
                 {
-                    data = JsonSerializer.Deserialize<List<SensorData>>(response.Value.GetString()) ?? [];
+                    // Fetch the historical sensor data from the state store
+                    StateStoreGetResponse response = await stateStoreClient.GetAsync(Constants.StateStoreSensorKey, null, cancellationToken);
+                    if (response.Value != null)
+                    {
+                        data = JsonSerializer.Deserialize<List<SensorData>>(response.Value.GetString()) ?? [];
+                    }
 
                     // Discard old data
                     DateTime timeNow = DateTime.UtcNow;
                     data.RemoveAll(d => timeNow - d.Timestamp > TimeSpan.FromSeconds(Constants.WindowSize));
                 }
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Failed to get state store data, deleting the key");
-                await stateStoreClient.DeleteAsync(Constants.StateStoreSensorKey, null, null);
-            }
+                catch (JsonException ex)
+                {
+                    logger.LogError(ex, "Unable to deserialize state store data, deleting the key");
+                    await stateStoreClient.DeleteAsync(Constants.StateStoreSensorKey, null, null, cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Failed to fetch state store data");
+                }
 
-            try
-            {
-                // Add the new sensor data
-                data.Add(sensor);
+                try
+                {
+                    // Drain the incoming queue
+                    do
+                    {
+                        data.Add(sensor);
+                    } while (incomingSensorData.TryTake(out sensor!));
 
-                // Push the sensor data back to the state store
-                await stateStoreClient.SetAsync(Constants.StateStoreSensorKey, JsonSerializer.Serialize(data), null, null);
-
-                logger.LogDebug("State store contains {count} items", data.Count);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, ex.Message);
+                    // Push the sensor data back to the state store
+                    await stateStoreClient.SetAsync(Constants.StateStoreSensorKey, JsonSerializer.Serialize(data), null, null, cancellationToken);
+                    logger.LogDebug("State store contains {count} items", data.Count);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, ex.Message);
+                }
             }
         }
     }
