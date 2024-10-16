746756c67f3c08720f37783617d82c333f18a8ad
timtay-microsoft <timtay@microsoft.com>
2024-10-15 17:51:47 -0700

    log

--- dotnet/samples/ConnectorApp/Worker.cs
+++ dotnet/samples/ConnectorApp/Worker.cs
@@ -59,8 +59,10 @@ namespace DotnetHttpConnectorWorkerService
                 using HttpDataRetriever httpDataRetriever = new(httpServerAssetEndpointProfile.TargetAddress, httpServerRequestPath, httpMethod, httpServerUsername, httpServerPassword);
 
                 // Create MQTT client from credentials provided by the operator
-                // Create MQTT client from credentials provided by the operator
                 MqttConnectionSettings mqttConnectionSettings = MqttConnectionSettings.FromFileMount();
+
+                _logger.LogInformation($"Connecting to MQTT broker with {mqttConnectionSettings}");
+
                 MqttSessionClient sessionClient = new();
                 await sessionClient.ConnectAsync(mqttConnectionSettings);
 
