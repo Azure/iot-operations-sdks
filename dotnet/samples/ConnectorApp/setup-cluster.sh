557101da609c18b9996bdd4569e1036d455cdce5
timtay-microsoft <timtay@microsoft.com>
2024-10-21 14:16:20 -0700

    pathing

--- dotnet/samples/ConnectorApp/setup-cluster.sh
+++ dotnet/samples/ConnectorApp/setup-cluster.sh
@@ -26,7 +26,7 @@ kubectl apply -f ./KubernetesResources/connector-config.yaml
 kubectl apply -f ./KubernetesResources/http-connector-secrets.yaml
 
 # Deploy HTTP server (as an asset)
-kubectl apply -f ./SampleHttpServer/http-server.yaml
+kubectl apply -f ./KubernetesResources/http-server.yaml
 
 # Deploy HTTP server asset and AEP
 kubectl apply -f ./KubernetesResources/http-server-asset-endpoint-profile-definition.yaml
