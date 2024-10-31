42d9a11438a8cbfdd65bae22348a90f768c41128
Oliva Kar <olkar@microsoft.com>
2024-10-31 11:01:51 -0700

    regenerated

--- dotnet/src/Azure.Iot.Operations.Services/Akri/DiscoveredAssetResourcesClient.cs
+++ dotnet/src/Azure.Iot.Operations.Services/Akri/DiscoveredAssetResourcesClient.cs
@@ -1,48 +1,48 @@
 ﻿
 
-//namespace Azure.Iot.Operations.Services.Akri;
+namespace Azure.Iot.Operations.Services.Akri;
 
-//using Azure.Iot.Operations.Protocol;
-//using Azure.Iot.Operations.Services.Akri.dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1;
+using Azure.Iot.Operations.Protocol;
+using Azure.Iot.Operations.Services.Akri.dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1;
 
-//using AssetEndpointProfileResponseInfo = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAssetEndpointProfile_Response;
-//using AssetResponseInfo = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Response;
+using AssetEndpointProfileResponseInfo = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAssetEndpointProfile_Response;
+using AssetResponseInfo = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Response;
 
-//public class DiscoveredAssetResourcesClient(IMqttPubSubClient pubSubClient) : IDiscoveredAssetResourcesClient
-//{
-//    private static readonly TimeSpan s_DefaultCommandTimeout = TimeSpan.FromSeconds(10);
-//    private readonly DiscoveredAssetResourcesClientStub _clientStub = new(pubSubClient);
-//    private bool _disposed;
+public class DiscoveredAssetResourcesClient(IMqttPubSubClient pubSubClient) : IDiscoveredAssetResourcesClient
+{
+    private static readonly TimeSpan s_DefaultCommandTimeout = TimeSpan.FromSeconds(10);
+    private readonly DiscoveredAssetResourcesClientStub _clientStub = new(pubSubClient);
+    private bool _disposed;
 
-//    public async Task<AssetEndpointProfileResponseInfo?> CreateDiscoveredAssetEndpointProfileAsync(
-//        CreateDiscoveredAssetEndpointProfileRequestPayload discoveredAssetEndpointProfileCommandRequest, TimeSpan? timeout = default, CancellationToken cancellationToken = default!)
-//    {
-//        cancellationToken.ThrowIfCancellationRequested();
-//        ObjectDisposedException.ThrowIf(_disposed, this);
+    public async Task<AssetEndpointProfileResponseInfo?> CreateDiscoveredAssetEndpointProfileAsync(
+        CreateDiscoveredAssetEndpointProfileRequestPayload discoveredAssetEndpointProfileCommandRequest, TimeSpan? timeout = default, CancellationToken cancellationToken = default!)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        ObjectDisposedException.ThrowIf(_disposed, this);
 
-//        return (await _clientStub.CreateDiscoveredAssetEndpointProfileAsync(
-//            discoveredAssetEndpointProfileCommandRequest, null, timeout ?? s_DefaultCommandTimeout, cancellationToken)).CreateDiscoveredAssetEndpointProfileResponse;
-//    }
+        return (await _clientStub.CreateDiscoveredAssetEndpointProfileAsync(
+            discoveredAssetEndpointProfileCommandRequest, null, timeout ?? s_DefaultCommandTimeout, cancellationToken)).CreateDiscoveredAssetEndpointProfileResponse;
+    }
 
-//    public async Task<AssetResponseInfo?> CreateDiscoveredAssetAsync(
-//        CreateDiscoveredAssetRequestPayload discoveredAssetCommandRequest, TimeSpan? timeout = default, CancellationToken cancellationToken = default!)
-//    {
-//        cancellationToken.ThrowIfCancellationRequested();
-//        ObjectDisposedException.ThrowIf(_disposed, this);
+    public async Task<AssetResponseInfo?> CreateDiscoveredAssetAsync(
+        CreateDiscoveredAssetRequestPayload discoveredAssetCommandRequest, TimeSpan? timeout = default, CancellationToken cancellationToken = default!)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        ObjectDisposedException.ThrowIf(_disposed, this);
 
-//        return (await _clientStub.CreateDiscoveredAssetAsync(discoveredAssetCommandRequest, null, timeout ?? s_DefaultCommandTimeout, cancellationToken)).CreateDiscoveredAssetResponse;
-//    }
+        return (await _clientStub.CreateDiscoveredAssetAsync(discoveredAssetCommandRequest, null, timeout ?? s_DefaultCommandTimeout, cancellationToken)).CreateDiscoveredAssetResponse;
+    }
 
-//    public async ValueTask DisposeAsync()
-//    {
-//        if (_disposed)
-//        {
-//            return;
-//        }
+    public async ValueTask DisposeAsync()
+    {
+        if (_disposed)
+        {
+            return;
+        }
 
-//        await _clientStub.DisposeAsync().ConfigureAwait(false);
-//        GC.SuppressFinalize(this);
-//        _disposed = true;
-//    }
-//}
+        await _clientStub.DisposeAsync().ConfigureAwait(false);
+        GC.SuppressFinalize(this);
+        _disposed = true;
+    }
+}
 
--- dotnet/src/Azure.Iot.Operations.Services/Akri/IDiscoveredAssetResourcesClient.cs
+++ dotnet/src/Azure.Iot.Operations.Services/Akri/IDiscoveredAssetResourcesClient.cs
@@ -1,20 +1,20 @@
-﻿//namespace Azure.Iot.Operations.Services.Akri;
+﻿namespace Azure.Iot.Operations.Services.Akri;
 
-//using AssetEndpointProfileResponseInfo = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAssetEndpointProfile_Response;
-//using AssetEndpointProfileRequestAuthMethodSchema = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Enum_CreateDiscoveredAssetEndpointProfile_Request_SupportedAuthenticationMethods_ElementSchema;
-//using AssetResponseInfo = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Response;
-//using AssetRequestDatasetsElementSchema = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Request_Datasets_ElementSchema;
-//using AssetRequestDefaultTopic = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Request_DefaultTopic;
-//using AssetRequestEventsSchema = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Request_Events_ElementSchema;
-//using Azure.Iot.Operations.Services.Akri.dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1;
+using AssetEndpointProfileResponseInfo = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAssetEndpointProfile_Response;
+using AssetEndpointProfileRequestAuthMethodSchema = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Enum_CreateDiscoveredAssetEndpointProfile_Request_SupportedAuthenticationMethods_ElementSchema;
+using AssetResponseInfo = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Response;
+using AssetRequestDatasetsElementSchema = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Request_Datasets_ElementSchema;
+using AssetRequestDefaultTopic = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Request_DefaultTopic;
+using AssetRequestEventsSchema = dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1.Object_CreateDiscoveredAsset_Request_Events_ElementSchema;
+using Azure.Iot.Operations.Services.Akri.dtmi_com_microsoft_deviceregistry_DiscoveredAssetResources__1;
 
-//public interface IDiscoveredAssetResourcesClient : IAsyncDisposable
-//{
-//    public Task<AssetEndpointProfileResponseInfo?> CreateDiscoveredAssetEndpointProfileAsync(
-//        CreateDiscoveredAssetEndpointProfileRequestPayload discoveredAssetEndpointProfileCommandRequest,
-//        TimeSpan? timeout = default!, CancellationToken cancellationToken = default!);
-//    public Task<AssetResponseInfo?> CreateDiscoveredAssetAsync(
-//        CreateDiscoveredAssetRequestPayload discoveredAssetCommandRequest,
-//        TimeSpan? timeout = default!, CancellationToken cancellationToken = default!);
-//}
+public interface IDiscoveredAssetResourcesClient : IAsyncDisposable
+{
+    public Task<AssetEndpointProfileResponseInfo?> CreateDiscoveredAssetEndpointProfileAsync(
+        CreateDiscoveredAssetEndpointProfileRequestPayload discoveredAssetEndpointProfileCommandRequest,
+        TimeSpan? timeout = default!, CancellationToken cancellationToken = default!);
+    public Task<AssetResponseInfo?> CreateDiscoveredAssetAsync(
+        CreateDiscoveredAssetRequestPayload discoveredAssetCommandRequest,
+        TimeSpan? timeout = default!, CancellationToken cancellationToken = default!);
+}
 
