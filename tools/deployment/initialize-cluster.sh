7d8005c021f08f77f77636cefe2fdcfdbbcbf8bb
Ryan Winter <ryanwinter@outlook.com>
2024-11-02 17:53:18 +0000

    install k9s with initialize cluster, also print output afterwards on how to enable non-root access to cluster
    Signed-off-by: Ryan Winter <ryanwinter@outlook.com>

--- tools/deployment/initialize-cluster.sh
+++ tools/deployment/initialize-cluster.sh
@@ -4,28 +4,35 @@ set -o errexit
 set -o pipefail
 
 # install k3d
-if [ ! $(which k3d) ] 
+if [ ! $(which k3d) ]
 then
     wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 fi
 
 # install Step
-if [ ! $(which step) ] 
+if [ ! $(which step) ]
 then
     wget https://dl.smallstep.com/cli/docs-cli-install/latest/step-cli_amd64.deb -P /tmp
     dpkg -i /tmp/step-cli_amd64.deb
 fi
 
 # install az cli
-if [ ! $(which az) ] 
+if [ ! $(which az) ]
 then
     curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 fi
 
 # install kubectl
-if [ ! $(which kubectl) ] 
+if [ ! $(which kubectl) ]
 then
     az aks install-cli
+fi
+
+# install k9s
+if [ ! $(which k9s) ]
+then
+    wget https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb -P /tmp
+    dpkg -i /tmp/k9s_linux_amd64.deb
 fi
 
 # Create k3d cluster and forwarded ports (MQTT/MQTTS)
@@ -39,3 +46,11 @@ k3d cluster create \
 
 # Set the default context / namespace to azure-iot-operations
 kubectl config set-context k3d-k3s-default --namespace=azure-iot-operations
+
+echo
+echo =================================================================================================
+echo The k3d cluster has been created and the default context has been set to azure-iot-operations.
+echo If you need non-root access to the cluster, run the following command:
+echo
+echo "mkdir ~/.kube; sudo install -o $USER -g $USER /root/.kube/config ~/.kube/config"
+echo =================================================================================================
