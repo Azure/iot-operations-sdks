766ab421f0e62b3a52db8fcb585613c04bd342cc
Ryan Winter <ryanwinter@outlook.com>
2024-11-19 22:08:58 +0000

    update scripts
    Signed-off-by: GitHub <noreply@github.com>

--- tools/deployment/deploy-aio.sh
+++ tools/deployment/deploy-aio.sh
@@ -19,7 +19,7 @@ session_dir=$script_dir/../../.session
 mkdir -p $session_dir
 cd $script_dir
 
-# add/upgrade the azure-iot-ops / connectedk8s extensions
+# add & upgrade the extensions
 az extension add --upgrade --name azure-iot-ops
 az extension add --upgrade --name connectedk8s
 
@@ -36,15 +36,17 @@ if [ "$deploy_type" = "nightly" ]; then
 
     # install AIO Broker
     helm uninstall broker -n azure-iot-operations --ignore-not-found
-    helm install broker --atomic --create-namespace -n azure-iot-operations --version 1.0.0-hughes oci://mqbuilds.azurecr.io/helm/aio-broker --wait
+    helm install broker --atomic --create-namespace -n azure-iot-operations --version 1.1.0-dev oci://mqbuilds.azurecr.io/helm/aio-broker --wait
 fi
 
 # create root & intermediate CA
-step certificate create --profile root-ca "my root ca" --no-password --insecure \
-    $session_dir/root_ca.crt $session_dir/root_ca.key
-step certificate create --profile intermediate-ca "my intermediate ca" --no-password --insecure \
+step certificate create --profile root-ca "my root ca" \
+    $session_dir/root_ca.crt $session_dir/root_ca.key \
+    --no-password --insecure
+step certificate create --profile intermediate-ca "my intermediate ca" \
     $session_dir/intermediate_ca.crt $session_dir/intermediate_ca.key \
-    --ca $session_dir/root_ca.crt --ca-key $session_dir/root_ca.key --no-password --insecure
+    --ca $session_dir/root_ca.crt --ca-key $session_dir/root_ca.key \
+    --no-password --insecure
 
 # create client trust bundle used to validate x509 client connections to the broker
 kubectl delete configmap client-ca-trust-bundle -n azure-iot-operations --ignore-not-found
@@ -56,20 +58,5 @@ kubectl apply -f yaml/aio-$deploy_type.yaml
 
 # Update the credentials locally for connecting to MQTT Broker
 ./update-credentials.sh
-
-# Add ADR
-helm install adr --version 0.2.0 oci://mcr.microsoft.com/azureiotoperations/helm/adr/assets-arc-extension -n azure-iot-operations --wait
-
-# Deploy the Akri Operator
-helm install akri-operator oci://akribuilds.azurecr.io/helm/microsoft-managed-akri-operator --version 0.4.0-main-20241101.1-buddy -n azure-iot-operations --wait
-
-# Add AKRI Service for 38884
-helm install akri oci://mcr.microsoft.com/azureiotoperations/helm/microsoft-managed-akri --version 0.5.8 \
---set agent.extensionService.mqttBroker.useTls=true \
---set agent.extensionService.mqttBroker.caCertConfigMapRef="azure-iot-operations-aio-ca-trust-bundle" \
---set agent.extensionService.mqttBroker.authenticationMethod=serviceAccountToken \
---set agent.extensionService.mqttBroker.hostName=aio-broker-external.azure-iot-operations.svc.cluster.local \
---set agent.extensionService.mqttBroker.port=38884 \
--n azure-iot-operations --wait
 
 echo Setup complete, session related files are in the '.session' directory
