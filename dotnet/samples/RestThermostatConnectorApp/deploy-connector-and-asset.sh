706f5fc1e987c0b866416119ecaac93a597e045c
timtay-microsoft <timtay@microsoft.com>
2024-11-19 11:16:04 -0800

    scripts

--- dotnet/samples/RestThermostatConnectorApp/deploy-connector-and-asset.sh
+++ dotnet/samples/RestThermostatConnectorApp/deploy-connector-and-asset.sh
@@ -11,8 +11,12 @@ k3d image import rest-server:latest -c k3s-default
 kubectl apply -f ./KubernetesResources/connector-config.yaml
 
 # Deploy REST server (as an asset)
-kubectl apply -f ./KubernetesResources/rest-server.yaml
+kubectl apply -f ./KubernetesResources/rest-server1.yaml
+kubectl apply -f ./KubernetesResources/rest-server2.yaml
 
 # Deploy REST server asset and AEP
-kubectl apply -f ./KubernetesResources/rest-server-asset-endpoint-profile-definition.yaml
-kubectl apply -f ./KubernetesResources/rest-server-asset-definition.yaml
+kubectl apply -f ./KubernetesResources/rest-server1-asset-endpoint-profile-definition.yaml
+kubectl apply -f ./KubernetesResources/rest-server1-asset-definition.yaml
+
+kubectl apply -f ./KubernetesResources/rest-server2-asset-endpoint-profile-definition.yaml
+kubectl apply -f ./KubernetesResources/rest-server2-asset-definition.yaml
