52fac2f2007718bfedaafec6e0b3f2eaaee2690b
Ryan Winter <ryanwinter@outlook.com>
2024-11-19 19:57:18 +0000

    clean up startup scripts more
    Signed-off-by: GitHub <noreply@github.com>

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -7,13 +7,13 @@ echo 'export SCHEMA_REGISTRY=${CLUSTER_NAME}-sr' >> ~/.bashrc
 echo 'export SCHEMA_REGISTRY_NAMESPACE=${CLUSTER_NAME}-srn' >> ~/.bashrc
 source ~/.bashrc
 
-echo -e "Environment:\n
-echo -e "\tSUBSCRIPTION_ID: $SUBSCRIPTION_ID"
-echo -e "\tRESOURCE_GROUP: $RESOURCE_GROUP"
-echo -e "\tLOCATION: $LOCATION"
-echo -e "\tCLUSTER_NAME: $CLUSTER_NAME"
-echo -e "\tSTORAGE_ACCOUNT: $STORAGE_ACCOUNT"
-echo -e "\tSCHEMA_REGISTRY: $SCHEMA_REGISTRY"
-echo -e "\tSCHEMA_REGISTRY_NAMESPACE: $SCHEMA_REGISTRY_NAMESPACE"
+echo "Environment:
+    SUBSCRIPTION_ID:           $SUBSCRIPTION_ID
+    RESOURCE_GROUP:            $RESOURCE_GROUP
+    LOCATION:                  $LOCATION
+    CLUSTER_NAME:              $CLUSTER_NAME
+    STORAGE_ACCOUNT:           $STORAGE_ACCOUNT
+    SCHEMA_REGISTRY:           $SCHEMA_REGISTRY
+    SCHEMA_REGISTRY_NAMESPACE: $SCHEMA_REGISTRY_NAMESPACE"
 
 sudo sh -c 'echo 127.0.0.1 aio-broker >> /etc/hosts'
