605380dfd6f3b358516b4ee687e18fbbaad1e39a
timtay-microsoft <timtay@microsoft.com>
2024-11-19 14:43:40 -0800

    derp

--- dotnet/src/Azure.Iot.Operations.Connector/ConnectorWorker.cs
+++ dotnet/src/Azure.Iot.Operations.Connector/ConnectorWorker.cs
@@ -90,7 +90,7 @@ namespace Azure.Iot.Operations.Connector
                         if (assetEndpointProfile.AdditionalConfiguration != null)
                         {
                             _logger.LogInformation("####1");
-                            _logger.LogInformation($"{assetEndpointProfile.AdditionalConfiguration.ToString()}")
+                            _logger.LogInformation($"{assetEndpointProfile.AdditionalConfiguration.ToString()}");
                         }
 
                         if (assetEndpointProfile.AdditionalConfiguration.RootElement.TryGetProperty("leadershipPositionId", out JsonElement value))
