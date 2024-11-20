1d56674d0e54d4a3660f454fc8a5450ce5cf98de
Ryan Winter <ryanwinter@outlook.com>
2024-11-20 18:54:23 +0000

    update akri operator helm
    Signed-off-by: GitHub <noreply@github.com>

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# create some environment variables to simplify deployment
 export BASE_NAME=`echo ${CODESPACE_NAME//-} | head -c 12`
+
+# create environment variables to support deployment
 echo "export CLUSTER_NAME=${BASE_NAME}
 export STORAGE_ACCOUNT=${BASE_NAME}storage
 export SCHEMA_REGISTRY=${BASE_NAME}schema
 export SCHEMA_REGISTRY_NAMESPACE=${BASE_NAME}schemans
 export SESSION=${CODESPACE_VSCODE_FOLDER}/.session" >> ~/.bashrc
+
+# create a default resource group if not defined
+if [ -z "$RESOURCE_GROUP" ]; then
+    echo "export RESOURCE_GROUP=aio-${BASE_NAME}" >> ~/.bashrc
+fi
+
+# create a default location if not defined
+if [ -z "$LOCATION" ]; then
+    echo "export LOCATION=westus3" >> ~/.bashrc
+fi
 
 source ~/.bashrc
 
--- tools/deployment/install-arc-aio.sh
+++ tools/deployment/install-arc-aio.sh
@@ -2,15 +2,20 @@
 
 set -o errexit # fail if any command fails
 
-# Arc
-az login
+# arc
+echo ===Installing Azure Arc===
 az connectedk8s connect --name $CLUSTER_NAME --location $LOCATION --resource-group $RESOURCE_GROUP
+echo ===Enabling Azure Arc Features===
 az connectedk8s enable-features -n $CLUSTER_NAME -g $RESOURCE_GROUP --features cluster-connect custom-locations
 
-# Schema registry
+# schema registry
+echo ===Creating Storage Account===
 az storage account create --name $STORAGE_ACCOUNT --location $LOCATION --resource-group $RESOURCE_GROUP --enable-hierarchical-namespace
+echo ===Creating Schema Registry===
 az iot ops schema registry create --name $SCHEMA_REGISTRY --resource-group $RESOURCE_GROUP --registry-namespace $SCHEMA_REGISTRY_NAMESPACE --sa-resource-id $(az storage account show --name $STORAGE_ACCOUNT -o tsv --query id)
 
-# Azure IoT Operations
+# azure iot operations
+echo ===Initializing Azure IoT Operations===
 az iot ops init --cluster $CLUSTER_NAME --resource-group $RESOURCE_GROUP
-az iot ops create --cluster $CLUSTER_NAME --resource-group $RESOURCE_GROUP --name ${CLUSTER_NAME}-instance --sr-resource-id $(az iot ops schema registry show --name $SCHEMA_REGISTRY --resource-group $RESOURCE_GROUP -o tsv --query id) --fr 1 --fw 1 --bp 1 --bw 1 --br 2 --mp Low
+echo ===Creating Azure IoT Operations===
+az iot ops create --cluster $CLUSTER_NAME --resource-group $RESOURCE_GROUP --name ${CLUSTER_NAME}-instance  --sr-resource-id $(az iot ops schema registry show --name $SCHEMA_REGISTRY --resource-group $RESOURCE_GROUP -o tsv --query id) --broker-frontend-replicas 1 --broker-frontend-workers 1  --broker-backend-part 1  --broker-backend-workers 1 --broker-backend-rf 2 --broker-mem-profile Low
