0e2aff00a332e22d8fec44371d22d585d82087fb
Ryan Winter <ryanwinter@outlook.com>
2024-11-21 22:19:27 +0000

    reorder exports
    Signed-off-by: GitHub <noreply@github.com>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,11 +5,11 @@ echo "Starting postCreateCommand"
 BASE_NAME=`echo ${CODESPACE_NAME//-} | head -c 12`
 
 # create environment variables to support deployment
-echo "export CLUSTER_NAME=${BASE_NAME}
+echo "export SESSION=$PWD/.session
+export CLUSTER_NAME=${BASE_NAME}
 export STORAGE_ACCOUNT=${BASE_NAME}storage
 export SCHEMA_REGISTRY=${BASE_NAME}schema
-export SCHEMA_REGISTRY_NAMESPACE=${BASE_NAME}schemans
-export SESSION=$PWD/.session" >> ~/.bashrc
+export SCHEMA_REGISTRY_NAMESPACE=${BASE_NAME}schemans" >> ~/.bashrc
 
 # create a default resource group if not defined
 if [ -z "$RESOURCE_GROUP" ]; then
