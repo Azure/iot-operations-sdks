1db4acff63d14d2dca873b806ca6ba32a12e4459
Ryan Winter <ryanwinter@outlook.com>
2024-11-21 19:03:52 +0000

    convert bashrc variables to export so they are available for child scripts.
    Signed-off-by: GitHub <noreply@github.com>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,20 +5,20 @@ echo "Starting postCreateCommand"
 BASE_NAME=`echo ${CODESPACE_NAME//-} | head -c 12`
 
 # create environment variables to support deployment
-echo "CLUSTER_NAME=${BASE_NAME}
-STORAGE_ACCOUNT=${BASE_NAME}storage
-SCHEMA_REGISTRY=${BASE_NAME}schema
-SCHEMA_REGISTRY_NAMESPACE=${BASE_NAME}schemans
-SESSION=${CODESPACE_VSCODE_FOLDER}/.session" >> ~/.bashrc
+echo "export CLUSTER_NAME=${BASE_NAME}
+export STORAGE_ACCOUNT=${BASE_NAME}storage
+export SCHEMA_REGISTRY=${BASE_NAME}schema
+export SCHEMA_REGISTRY_NAMESPACE=${BASE_NAME}schemans
+export SESSION=${CODESPACE_VSCODE_FOLDER}/.session" >> ~/.bashrc
 
 # create a default resource group if not defined
 if [ -z "$RESOURCE_GROUP" ]; then
-    echo "RESOURCE_GROUP=aio-${BASE_NAME}" >> ~/.bashrc
+    echo "export RESOURCE_GROUP=aio-${BASE_NAME}" >> ~/.bashrc
 fi
 
 # create a default location if not defined
 if [ -z "$LOCATION" ]; then
-    echo "LOCATION=westus3" >> ~/.bashrc
+    echo "export LOCATION=westus3" >> ~/.bashrc
 fi
 
 echo "Ending postCreateCommand"
