56832d5fc76d12daa19344845644f8ed252b737f
Auto Gen <johndo@microsoft.com>
2024-11-21 09:50:52 -0800

    split CommonOptions into CommandOptions and TelemetryOptions

--- codegen/res/rust/common/common_types/common_options.rs
+++ codegen/res/rust/common/common_types/common_options.rs
@@ -6,7 +6,18 @@ use derive_builder::Builder;
 
 #[allow(unused)]
 #[derive(Builder, Clone)]
-pub struct CommonOptions {
+pub struct CommandOptions {
+    /// Optional Topic namespace to be prepended to the topic pattern
+    #[builder(default = "None")]
+    pub topic_namespace: Option<String>,
+    /// Topic token keys/values to be replaced in the topic pattern
+    #[builder(default)]
+    pub topic_token_map: HashMap<String, String>,
+}
+
+#[allow(unused)]
+#[derive(Builder, Clone)]
+pub struct TelemetryOptions {
     /// Optional Topic namespace to be prepended to the topic pattern
     #[builder(default = "None")]
     pub topic_namespace: Option<String>,
@@ -15,5 +26,5 @@ pub struct CommonOptions {
     pub topic_token_map: HashMap<String, String>,
     /// If true, telemetry messages are auto-acknowledged when received
     #[builder(default = "true")]
-    pub auto_ack_telemetry: bool,
+    pub auto_ack: bool,
 }
