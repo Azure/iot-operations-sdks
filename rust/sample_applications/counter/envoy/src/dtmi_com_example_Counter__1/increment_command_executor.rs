2ce0675ecf8adee0ebf0756808e49a543f189404
vaavva <vaavva@microsoft.com>
2024-11-25 16:26:29 -0500

    use debug version of codegen so we can use sdkPath

--- rust/sample_applications/counter/envoy/src/dtmi_com_example_Counter__1/increment_command_executor.rs
+++ rust/sample_applications/counter/envoy/src/dtmi_com_example_Counter__1/increment_command_executor.rs
@@ -13,7 +13,7 @@ use azure_iot_operations_protocol::rpc::command_executor::{
 use super::increment_response_payload::IncrementResponsePayload;
 use super::MODEL_ID;
 use super::REQUEST_TOPIC_PATTERN;
-use crate::common_types::common_options::CommonOptions;
+use crate::common_types::common_options::CommandOptions;
 use crate::common_types::empty_json::EmptyJson;
 
 pub type IncrementRequest = CommandRequest<EmptyJson, IncrementResponsePayload>;
@@ -70,7 +70,7 @@ where
     ///
     /// # Panics
     /// If the DTDL that generated this code was invalid
-    pub fn new(client: C, options: &CommonOptions) -> Self {
+    pub fn new(client: C, options: &CommandOptions) -> Self {
         let mut executor_options_builder = CommandExecutorOptionsBuilder::default();
         if let Some(topic_namespace) = &options.topic_namespace {
             executor_options_builder.topic_namespace(topic_namespace.clone());
--- rust/sample_applications/counter/envoy/src/dtmi_com_example_Counter__1/read_counter_command_invoker.rs
+++ rust/sample_applications/counter/envoy/src/dtmi_com_example_Counter__1/read_counter_command_invoker.rs
@@ -14,7 +14,7 @@ use azure_iot_operations_protocol::rpc::command_invoker::{
 use super::read_counter_response_payload::ReadCounterResponsePayload;
 use super::MODEL_ID;
 use super::REQUEST_TOPIC_PATTERN;
-use crate::common_types::common_options::CommonOptions;
+use crate::common_types::common_options::CommandOptions;
 use crate::common_types::empty_json::EmptyJson;
 
 pub type ReadCounterRequest = CommandRequest<EmptyJson>;
@@ -88,7 +88,7 @@ where
     ///
     /// # Panics
     /// If the DTDL that generated this code was invalid
-    pub fn new(client: C, options: &CommonOptions) -> Self {
+    pub fn new(client: C, options: &CommandOptions) -> Self {
         let mut invoker_options_builder = CommandInvokerOptionsBuilder::default();
         if let Some(topic_namespace) = &options.topic_namespace {
             invoker_options_builder.topic_namespace(topic_namespace.clone());
