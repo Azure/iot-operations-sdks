e4e2d52ac5b600529577fd49b8fea888b8e37ffc
RLeclair <raulleclair@microsoft.com>
2024-12-06 09:50:54 -0800

    Dtdl + Script

---
+++ rust/azure_iot_operations_services/gen.sh
@@ -0,0 +1,2 @@
+../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net8.0/Azure.Iot.Operations.ProtocolCompiler \
+ --clientOnly --modelFile ../../eng/dtdl/schemaregistry.json --sdkPath ../ --lang=rust
