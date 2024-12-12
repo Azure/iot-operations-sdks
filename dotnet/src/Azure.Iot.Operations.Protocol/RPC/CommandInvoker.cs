484a932f61da0db5c18982157fa168b9f54cc9f6
Oliva Kar <olkar@microsoft.com>
2024-12-12 10:22:24 -0800

    comment

--- dotnet/src/Azure.Iot.Operations.Protocol/RPC/CommandInvoker.cs
+++ dotnet/src/Azure.Iot.Operations.Protocol/RPC/CommandInvoker.cs
@@ -227,23 +227,23 @@ namespace Azure.Iot.Operations.Protocol.RPC
                         return Task.CompletedTask;
                     }
 
-                    if (!supportedMajorProtocolVersions.Contains(protocolVersion!.MajorVersion))
-                    {
-                        AkriMqttException akriException = new($"Received a response with an unsupported protocol version number: {responseProtocolVersion}")
-                        {
-                            Kind = AkriMqttErrorKind.UnsupportedResponseVersion,
-                            InApplication = false,
-                            IsShallow = false,
-                            IsRemote = false,
-                            CommandName = commandName,
-                            CorrelationId = requestGuid,
-                            SupportedMajorProtocolVersions = supportedMajorProtocolVersions,
-                            ProtocolVersion = responseProtocolVersion,
-                        };
+                    //if (!supportedMajorProtocolVersions.Contains(protocolVersion!.MajorVersion))
+                    //{
+                    //    AkriMqttException akriException = new($"Received a response with an unsupported protocol version number: {responseProtocolVersion}")
+                    //    {
+                    //        Kind = AkriMqttErrorKind.UnsupportedResponseVersion,
+                    //        InApplication = false,
+                    //        IsShallow = false,
+                    //        IsRemote = false,
+                    //        CommandName = commandName,
+                    //        CorrelationId = requestGuid,
+                    //        SupportedMajorProtocolVersions = supportedMajorProtocolVersions,
+                    //        ProtocolVersion = responseProtocolVersion,
+                    //    };
 
-                        SetExceptionSafe(responsePromise.CompletionSource, akriException);
-                        return Task.CompletedTask;
-                    }
+                    //    SetExceptionSafe(responsePromise.CompletionSource, akriException);
+                    //    return Task.CompletedTask;
+                    //}
 
                     MqttUserProperty? statusProperty = args.ApplicationMessage.UserProperties?.FirstOrDefault(p => p.Name == AkriSystemProperties.Status);
 
