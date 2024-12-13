37256aa2b685a72f1a9d6cb367cebce6529270a0
RLeclair <raulleclair@microsoft.com>
2024-12-13 10:40:09 -0800

    minor change

--- rust/azure_iot_operations_services/gen.sh
+++ rust/azure_iot_operations_services/gen.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 ../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net8.0/Azure.Iot.Operations.ProtocolCompiler \
  --clientOnly --modelFile ../../eng/dtdl/SchemaRegistry-1.json --sdkPath ../ --lang=rust
