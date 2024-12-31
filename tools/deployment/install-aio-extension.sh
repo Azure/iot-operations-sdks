ae74a13e1f0f6b8539084e38ea05f050fc138e27
timtay-microsoft <timtay@microsoft.com>
2024-12-31 10:38:42 -0800

    oid?

--- tools/deployment/install-aio-extension.sh
+++ tools/deployment/install-aio-extension.sh
@@ -14,7 +14,9 @@ if [ -z $SCHEMA_REGISTRY_NAMESPACE ]; then echo "SCHEMA_REGISTRY_NAMESPACE is no
 echo ===Installing Azure Arc===
 az connectedk8s connect --name $CLUSTER_NAME --location $LOCATION --resource-group $RESOURCE_GROUP
 echo ===Enabling Azure Arc Features===
-az connectedk8s enable-features -n $CLUSTER_NAME -g $RESOURCE_GROUP --features cluster-connect custom-locations
+OID="$(az ad sp show --id bc313c14-388c-4e7d-a58e-70017303ee3b --query id -o tsv)"
+echo $OID
+az connectedk8s enable-features -n $CLUSTER_NAME -g $RESOURCE_GROUP --custom-locations-oid $OID --features cluster-connect custom-locations
 
 # install schema registry
 echo ===Creating Storage Account===
@@ -27,6 +29,3 @@ echo ===Initializing Azure IoT Operations===
 az iot ops init --cluster $CLUSTER_NAME --resource-group $RESOURCE_GROUP
 echo ===Creating Azure IoT Operations===
 az iot ops create --cluster $CLUSTER_NAME --resource-group $RESOURCE_GROUP --name ${CLUSTER_NAME}-instance  --sr-resource-id $(az iot ops schema registry show --name $SCHEMA_REGISTRY --resource-group $RESOURCE_GROUP -o tsv --query id) --broker-frontend-replicas 1 --broker-frontend-workers 1  --broker-backend-part 1  --broker-backend-workers 1 --broker-backend-rf 2 --broker-mem-profile Low
-
-
-az ad sp show --id bc313c14-388c-4e7d-a58e-70017303ee3b --query id -o tsv
