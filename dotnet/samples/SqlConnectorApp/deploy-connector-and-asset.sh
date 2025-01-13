8c6518173907e4a19781067dbd68d62cbbd52e5d
olivakar <oliva.tanusree@gmail.com>
2025-01-13 18:43:16 +0000

    wip

--- dotnet/samples/SqlConnectorApp/deploy-connector-and-asset.sh
+++ dotnet/samples/SqlConnectorApp/deploy-connector-and-asset.sh
@@ -1,24 +1,25 @@
 # Build connector sample image
-# dotnet publish /t:PublishContainer
-# k3d image import restthermostatconnectorapp:latest -c k3s-default
-
-# Build REST server docker image
-# docker build -t rest-server:latest ./SampleRestServer
-# docker tag rest-server:latest rest-server:latest
-# k3d image import rest-server:latest -c k3s-default
+dotnet publish /t:PublishContainer
+k3d image import restthermostatconnectorapp:latest -c k3s-default
 
 # Deploy connector config
-# kubectl apply -f ./KubernetesResources/connector-config.yaml
+kubectl apply -f ./KubernetesResources/connector-config.yaml
 
-# Deploy REST server (as an asset)
-# kubectl apply -f ./KubernetesResources/rest-server.yaml
+# Deploy SQL server (as an asset)
+kubectl apply -f ./KubernetesResources/sql-server.yaml
 
-# Deploy REST server asset and AEP
+# if the sql server yaml cant insert data into the table
+# then it needs to be done manually. Port forward needs to be done before.
+# kubectl port-forward <pod-name> 1433:1433 
+# sqlcmd -U sa -P "MyExtremelyStrongpassword@123" 
+
+# For cretaing table and columns 
+sqlcmd -S 127.0.0.1 -U sa -P "MyExtremelyStrongpassword@123" -i setup.sql 
+
+ 
+
+# Deploy SQL server asset and AEP
 kubectl delete -f connector-config.yaml
 kubectl delete -f sql-server-asset-endpoint-profile-definition.yaml
 kubectl delete -f sql-server-asset-definition.yaml
 
-
-kubectl apply -f connector-config.yaml
-kubectl apply -f sql-server-asset-endpoint-profile-definition.yaml
-kubectl apply -f sql-server-asset-definition.yaml
