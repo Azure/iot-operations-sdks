0d093c696cec6a39c15751e0abf7c747146c6d94
Auto Gen <johndo@microsoft.com>
2025-01-14 14:38:44 -0800

    use TypeGenerator instead of avrogen for C# AVRO codegen

--- codegen/test/communication/gen.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-[[ -d ./dotnet/ProtocolCompiler.CommunicationTests/JsonComm ]] && rm -r ./dotnet/ProtocolCompiler.CommunicationTests/JsonComm
-../../src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net8.0/Azure.Iot.Operations.ProtocolCompiler --modelFile ./dtdl/JsonModel.json --outDir ./dotnet/ProtocolCompiler.CommunicationTests/JsonComm --lang csharp --sdkPath ../../../dotnet/src/Azure.Iot.Operations.Protocol
-
-[[ -d ./dotnet/ProtocolCompiler.CommunicationTests/AvroComm ]] && rm -r ./dotnet/ProtocolCompiler.CommunicationTests/AvroComm
-../../src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net8.0/Azure.Iot.Operations.ProtocolCompiler --modelFile ./dtdl/AvroModel.json --outDir ./dotnet/ProtocolCompiler.CommunicationTests/AvroComm --lang csharp --sdkPath ../../../dotnet/src/Azure.Iot.Operations.Protocol
