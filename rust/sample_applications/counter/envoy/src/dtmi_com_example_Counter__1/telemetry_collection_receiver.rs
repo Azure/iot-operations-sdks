9282c601ea338f05ffe381a0bfc1cd0ef2113b53
RLeclair <raulleclair@microsoft.com>
2025-01-14 13:39:31 -0800

    Codegen edit AckToken

--- rust/sample_applications/counter/envoy/src/dtmi_com_example_Counter__1/telemetry_collection_receiver.rs
+++ rust/sample_applications/counter/envoy/src/dtmi_com_example_Counter__1/telemetry_collection_receiver.rs
@@ -2,8 +2,7 @@
 
 use std::collections::HashMap;
 
-use azure_iot_operations_mqtt::interface::AckToken;
-use azure_iot_operations_mqtt::interface::ManagedClient;
+use azure_iot_operations_mqtt::interface::{AckToken, ManagedClient};
 use azure_iot_operations_protocol::common::aio_protocol_error::AIOProtocolError;
 use azure_iot_operations_protocol::telemetry::telemetry_receiver::{
     TelemetryMessage, TelemetryReceiver, TelemetryReceiverOptionsBuilder,
