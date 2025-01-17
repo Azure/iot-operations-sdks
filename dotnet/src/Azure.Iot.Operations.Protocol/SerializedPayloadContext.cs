2e82e30f1e8cc28dad56707680754147c7aa899b
timtay-microsoft <timtay@microsoft.com>
2025-01-17 14:38:54 -0800

    headers

--- dotnet/src/Azure.Iot.Operations.Protocol/SerializedPayloadContext.cs
+++ dotnet/src/Azure.Iot.Operations.Protocol/SerializedPayloadContext.cs
@@ -5,12 +5,27 @@ using Azure.Iot.Operations.Protocol.Models;
 
 namespace Azure.Iot.Operations.Protocol
 {
-    public class SerializedPayloadContext(byte[]? serializedPayload, string? contentType, MqttPayloadFormatIndicator payloadFormatIndicator = MqttPayloadFormatIndicator.Unspecified)
+    /// <summary>
+    /// A bundle of the payload that was serialized alongside the content type and payload format indicator used.
+    /// </summary>
+    /// <param name="serializedPayload">The serialized payload. May be null if no payload was serialized.</param>
+    /// <param name="contentType">The content type of the serialized payload.  May be null if no payload was serialized.</param>
+    /// <param name="payloadFormatIndicator">The payload format indicator of the serialized payload.  Should be "Unspecified" if no payload was serialized.</param>
+    public class SerializedPayloadContext(byte[]? serializedPayload, string? contentType, MqttPayloadFormatIndicator payloadFormatIndicator)
     {
+        /// <summary>
+        /// The serialized payload. May be null if no payload was serialized.
+        /// </summary>
         public byte[]? SerializedPayload { get; set; } = serializedPayload;
 
+        /// <summary>
+        /// The content type of the serialized payload.  May be null if no payload was serialized.
+        /// </summary>
         public string? ContentType { get; set; } = contentType;
 
+        /// <summary>
+        /// The payload format indicator of the serialized payload.  Should be "Unspecified" if no payload was serialized.
+        /// </summary>
         public MqttPayloadFormatIndicator PayloadFormatIndicator { get; set; } = payloadFormatIndicator;
     }
 }
