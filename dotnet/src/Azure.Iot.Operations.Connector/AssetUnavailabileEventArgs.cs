2c6c882bddff92e8dca7052241e29b3fec401841
timtay-microsoft <timtay@microsoft.com>
2025-02-03 09:46:48 -0800

    documentation

--- dotnet/src/Azure.Iot.Operations.Connector/AssetUnavailabileEventArgs.cs
+++ dotnet/src/Azure.Iot.Operations.Connector/AssetUnavailabileEventArgs.cs
@@ -4,11 +4,17 @@
 
 namespace Azure.Iot.Operations.Connector
 {
+    /// <summary>
+    /// The event args for when an asset becomes unavailable to sample.
+    /// </summary>
     public class AssetUnavailabileEventArgs : EventArgs
     {
+        /// <summary>
+        /// The name of the asset that is no longer available to sample.
+        /// </summary>
         public string AssetName { get; }
 
-        public AssetUnavailabileEventArgs(string assetName)
+        internal AssetUnavailabileEventArgs(string assetName)
         {
             AssetName = assetName;
         }
