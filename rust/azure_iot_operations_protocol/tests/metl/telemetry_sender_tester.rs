61e0b0e2930bd03e961bcc0f1eec4ff8420bda00
Auto Gen <johndo@microsoft.com>
2025-02-11 09:30:13 -0800

    Add content-type and format-indicator to METL PublishedMessage

--- rust/azure_iot_operations_protocol/tests/metl/telemetry_sender_tester.rs
+++ rust/azure_iot_operations_protocol/tests/metl/telemetry_sender_tester.rs
@@ -457,7 +457,9 @@ where
             if let Some(properties) = published_message.properties.as_ref() {
                 assert_eq!(expected_message.content_type, properties.content_type);
             } else {
-                panic!("expected content type but found no properties in published message");
+                panic!(
+                    "expected content type but found no properties in published message"
+                );
             }
         }
 
