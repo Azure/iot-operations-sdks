5523b482bbafc3f1a0557212e656de897cfadca4
Oliva Kar <olkar@microsoft.com>
2025-02-12 19:08:48 -0800

    again akri

--- dotnet/src/Azure.Iot.Operations.Services/Akri/DiscoveredAssetResourcesClient.cs
+++ dotnet/src/Azure.Iot.Operations.Services/Akri/DiscoveredAssetResourcesClient.cs
@@ -9,11 +9,17 @@ using Azure.Iot.Operations.Services.Akri.DiscoveredAssetResources;
 using AssetEndpointProfileResponseInfo = DiscoveredAssetResources.CreateDiscoveredAssetEndpointProfileResponseSchema;
 using AssetResponseInfo = DiscoveredAssetResources.CreateDiscoveredAssetResponseSchema;
 
-public class DiscoveredAssetResourcesClient(IMqttPubSubClient pubSubClient) : IDiscoveredAssetResourcesClient
+public class DiscoveredAssetResourcesClient : IDiscoveredAssetResourcesClient
 {
-    private readonly DiscoveredAssetResourcesClientStub _clientStub = new(pubSubClient);
+    private readonly ApplicationContext _applicationContext;
+    private readonly DiscoveredAssetResourcesClientStub _clientStub;
     private bool _disposed;
 
+    public DiscoveredAssetResourcesClient(IMqttPubSubClient pubSubClient)
+    {
+        _applicationContext = new ApplicationContext(); // Create a new application context. TODO - have kept it here to pass current tests.
+        _clientStub = new DiscoveredAssetResourcesClientStub(_applicationContext, pubSubClient); // Pass it to the stub
+    }
     public async Task<AssetEndpointProfileResponseInfo?> CreateDiscoveredAssetEndpointProfileAsync(
         CreateDiscoveredAssetEndpointProfileRequestPayload discoveredAssetEndpointProfileCommandRequest, TimeSpan? timeout = default, CancellationToken cancellationToken = default!)
     {
