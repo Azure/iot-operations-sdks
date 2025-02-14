a11245fffdf159941bb287e319547504b0c6a96f
timtay-microsoft <timtay@microsoft.com>
2025-02-14 12:18:59 -0800

    more

--- dotnet/samples/EventDrivenTcpThermostatConnector/deploy-connector-and-asset.sh
+++ dotnet/samples/EventDrivenTcpThermostatConnector/deploy-connector-and-asset.sh
@@ -4,14 +4,14 @@ k3d image import sampletcpclientapp:latest -c k3s-default
 
 # Build connector sample image
 dotnet publish /t:PublishContainer
-k3d image import eventdrivenrestthermostatconnector:latest -c k3s-default
+k3d image import eventdriventcpthermostatconnector:latest -c k3s-default
 
 # Deploy connector config
 kubectl apply -f ./KubernetesResources/connector-config.yaml
 
-# Deploy REST server (as an asset)
+# Deploy TCP server (as an asset)
 kubectl apply -f ./KubernetesResources/tcp-service.yaml
 
-# Deploy REST server asset and AEP
+# Deploy TCP server asset and AEP
 kubectl apply -f ./KubernetesResources/tcp-service-asset-endpoint-profile-definition.yaml
 kubectl apply -f ./KubernetesResources/tcp-service-asset-definition.yaml
