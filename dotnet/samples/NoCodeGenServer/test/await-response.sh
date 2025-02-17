9aa5db0bbcc8321bc4d4ebd56e3591cc3a5c306c
Alexander Köpke <alexander.koepke@microsoft.com>
2025-02-17 15:03:17 +0100

    Print payload of message as part of await-response script

--- dotnet/samples/NoCodeGenServer/test/await-response.sh
+++ dotnet/samples/NoCodeGenServer/test/await-response.sh
@@ -2,6 +2,6 @@ mosquitto_sub \
 -d  \
 -t "#" \
 -V mqttv5 \
--F "%%Topic: %t\n%%message-id: %m\n%%content-type: %C\n%%correlation-data %D\n%%user-properties: %P\n"
-# -F "%% Topic: %%t \n %% message-id: %%m\n%% content-type: %%C\n%% correlation-data: %%D\n%% user-properties: %%P\n"
+-F "#Topic: %t\n#message-id: %m\n#content-type: %C\n#correlation-data: %D\n#user-properties: %P\n#payload: %p\n"
+# -F "%% Topic: %%t \n %% message-id: %%m\n%% content-type: %%C\n%% correlation-data: %%D\n%% user-properties: %%P\n#"
 
