584b00bf91016146e71943f54e1701e7c011c90f
timtay-microsoft <timtay@microsoft.com>
2025-02-18 17:08:27 -0800

    bin path 8.0

--- go/services/schemaregistry/gen.sh
+++ go/services/schemaregistry/gen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ROOT=$(git rev-parse --show-toplevel)
-"$ROOT/codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler" \
+"$ROOT/codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net8.0/Azure.Iot.Operations.ProtocolCompiler" \
     --modelFile "$ROOT/eng/dtdl/SchemaRegistry-1.json" \
     --outDir "$ROOT/go/services/schemaregistry" \
     --clientOnly --lang go
