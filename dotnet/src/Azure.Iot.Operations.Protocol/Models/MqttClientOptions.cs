e765f2564d92e772bf6e606f64b9fdcb6b4f204c
timtay-microsoft <timtay@microsoft.com>
2025-02-18 18:31:47 -0800

    default

--- dotnet/src/Azure.Iot.Operations.Protocol/Models/MqttClientOptions.cs
+++ dotnet/src/Azure.Iot.Operations.Protocol/Models/MqttClientOptions.cs
@@ -238,7 +238,7 @@ namespace Azure.Iot.Operations.Protocol.Models
         ///         connect properly.
         ///     </remarks>
         /// </summary>
-        public bool TryPrivate { get; set; } = false;
+        public bool TryPrivate { get; set; }
 
         /// <summary>
         ///     Gets or sets the user properties.
