f1aa92e8f94aa85057c1fd20ae2ceb7809f1bb40
Auto Gen <johndo@microsoft.com>
2025-02-26 13:59:25 -0800

    rerun gen.sh and update as needed

--- rust/sample_applications/counter/envoy/src/counter/reset_command_invoker.rs
+++ rust/sample_applications/counter/envoy/src/counter/reset_command_invoker.rs
@@ -34,6 +34,20 @@ impl ResetRequestBuilder {
         self
     }
 
+    /// Topic token keys/values to be replaced into the publish topic of the request message.
+    /// A prefix of "ex:" will be prepended to each key before scanning the request topic pattern.
+    /// Thus, only tokens of the form `{ex:SOMEKEY}` will be replaced.
+    pub fn topic_tokens(&mut self, topic_tokens: Vec<(String, String)>) -> &mut Self {
+        self.inner_builder.topic_tokens(
+            topic_tokens
+                .clone()
+                .into_iter()
+                .map(|(k, v)| (format!("ex:{k}"), v))
+                .collect::<HashMap<String, String>>(),
+        );
+        self
+    }
+
     /// Timeout for the request
     pub fn timeout(&mut self, timeout: Duration) -> &mut Self {
         self.inner_builder.timeout(timeout);
