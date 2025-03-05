0528026b773ab408af86f479893dd84e52f2c558
timtay-microsoft <timtay@microsoft.com>
2025-03-04 18:13:40 -0800

    moot

--- dotnet/test/Azure.Iot.Operations.Protocol.IntegrationTests/CustomTopicTokenEnvoyTests.cs
+++ dotnet/test/Azure.Iot.Operations.Protocol.IntegrationTests/CustomTopicTokenEnvoyTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 using Azure.Iot.Operations.Mqtt.Session;
 
 namespace Azure.Iot.Operations.Protocol.IntegrationTests
