8606fad5f64023054f4485140fe25768595b594e
timtay-microsoft <timtay@microsoft.com>
2025-03-06 10:50:12 -0800

    Revert "debugging"
    This reverts commit 8087d23fe48b40aa0e89881ccf963ddd87b74447.

--- dotnet/src/Azure.Iot.Operations.Mqtt/Converters/GenericCertificateSelectionHandler.cs
+++ dotnet/src/Azure.Iot.Operations.Mqtt/Converters/GenericCertificateSelectionHandler.cs
@@ -17,14 +17,7 @@ namespace Azure.Iot.Operations.Mqtt.Converters
 
         public X509Certificate HandleCertificateSelection(MqttClientCertificateSelectionEventArgs args)
         {
-            try
-            {
-                return _mqttNetFunc.Invoke(new MQTTnet.MqttClientCertificateSelectionEventArgs(args.TargetHost, args.LocalCertificates, args.RemoteCertificate, args.AcceptableIssuers, MqttNetConverter.FromGeneric(args.TcpOptions)));
-            }
-            catch (NullReferenceException)
-            {
-                throw new NullReferenceException("func null? " + (_mqttNetFunc == null) + " args null? " + (args == null) + " tcpOptions null? " + (args?.TcpOptions == null));
-            }
+            return _mqttNetFunc.Invoke(new MQTTnet.MqttClientCertificateSelectionEventArgs(args.TargetHost, args.LocalCertificates, args.RemoteCertificate, args.AcceptableIssuers, MqttNetConverter.FromGeneric(args.TcpOptions)));
         }
     }
 }
