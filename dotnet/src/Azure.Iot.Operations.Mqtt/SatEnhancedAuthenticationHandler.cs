429979fe9c162e7154bf4ee1b9dd0325bc4d170f
timtay-microsoft <timtay@microsoft.com>
2025-03-11 15:45:42 -0700

    [.NET] Fix bug where MQTT client closed connection upon receiving MQTT auth packet in response to re-auth
    MQTTnet's client implementation will close the connection if no handler is set, even if the handler does nothing. This adds a handler by default that simply logs the received response status code
    
    https://github.com/dotnet/MQTTnet/blob/93a9d8d1dbe8dde2b7d
    79a37c873fae13b4f4797/Source/MQTTnet/MqttClient.cs#L660

---
+++ dotnet/src/Azure.Iot.Operations.Mqtt/SatEnhancedAuthenticationHandler.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics;
+using MQTTnet;
+
+namespace Azure.Iot.Operations.Mqtt
+{
+    internal class SatEnhancedAuthenticationHandler : IMqttEnhancedAuthenticationHandler
+    {
+        public Task HandleEnhancedAuthenticationAsync(MqttEnhancedAuthenticationEventArgs eventArgs)
+        {
+            if (eventArgs.ReasonCode == MQTTnet.Protocol.MqttAuthenticateReasonCode.Success)
+            {
+                Trace.TraceInformation("Received re-authentication response from MQTT broker with status {0}", eventArgs.ReasonCode);
+            }
+            else
+            {
+                Trace.TraceError("Received re-authentication response from MQTT broker with status {0} and reason string {1}", eventArgs.ReasonCode, eventArgs.ReasonString);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
