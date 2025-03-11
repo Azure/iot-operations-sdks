e51948098a0ecd7374af9c65e4d50ea51812f0eb
timtay-microsoft <timtay@microsoft.com>
2025-03-11 09:01:33 -0700

    refresh test

--- dotnet/test/Azure.Iot.Operations.Mqtt.UnitTests/TokenRefreshTimerTests.cs
+++ dotnet/test/Azure.Iot.Operations.Mqtt.UnitTests/TokenRefreshTimerTests.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Azure.Iot.Operations.Protocol;
+using Azure.Iot.Operations.Protocol.Models;
 using Azure.Iot.Operations.Protocol.UnitTests;
 using Microsoft.IdentityModel.Tokens;
 using Moq;
@@ -60,6 +61,39 @@ namespace Azure.Iot.Operations.Mqtt.UnitTests
             }
         }
 
+        [Fact]
+        public async Task TestTokenRefresh()
+        {
+            var mockMqttClient = new Mock<IMqttClient>();
+            mockMqttClient.Setup(mock => mock.IsConnected).Returns(true);
+            string fileName = Guid.NewGuid().ToString();
+            Directory.CreateDirectory("./TestFiles/");
+            File.WriteAllText("./TestFiles/" + fileName, GenerateJwtToken(DateTime.UtcNow.AddSeconds(8)));
+            try
+            {
+                TokenRefreshTimer tokenRefreshTimer = new(mockMqttClient.Object, "./TestFiles/" + fileName);
+                string newToken = GenerateJwtToken(DateTime.UtcNow.AddMinutes(60));
+                File.WriteAllText("./TestFiles/" + fileName, newToken); // refresh the token on disk
+                await Task.Delay(TimeSpan.FromSeconds(6)); // wait a bit for the TokenRefreshTimer to run it's periodic renewal task
+                mockMqttClient.Verify(
+                    mock =>
+                        mock.SendEnhancedAuthenticationExchangeDataAsync(
+                            It.Is<MqttEnhancedAuthenticationExchangeData>(data => Enumerable.SequenceEqual(data.AuthenticationData!, Encoding.UTF8.GetBytes(newToken))),
+                            It.Is<CancellationToken>(token => token == default)),
+                    Times.Once());
+            }
+            finally
+            {
+                try
+                {
+                    File.Delete("./TestFiles/" + fileName);
+                }
+                catch (Exception)
+                {
+                    // It's fine if deleting this file fails
+                }
+            }
+        }
         public string GenerateJwtToken(DateTime expiry)
         {
             // Test credentials. Do not use in any production setting
