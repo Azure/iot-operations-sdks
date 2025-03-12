56c0d5fb1256e649f2b0f1d7a9dcbb168dd56794
Carl LeCompte <clecompt@microsoft.com>
2025-03-12 15:55:36 -0700

    [Go] Leased-lock renew v2

--- go/services/leasedlock/options.go
+++ go/services/leasedlock/options.go
@@ -15,16 +15,21 @@ type (
 
 	// Options are the resolved options for the lock requests.
 	Options struct {
-		Timeout   time.Duration
-		SessionID string
+		Timeout time.Duration
+		Session string
+		Renew   time.Duration
 	}
 
 	// WithTimeout adds a timeout to the request (with second precision).
 	WithTimeout time.Duration
 
-	// WithSessionID adds an optional session ID suffix to the lock holder to
+	// WithSession adds an optional session ID suffix to the lock holder to
 	// allow distinct locks on the same key with the same MQTT client.
-	WithSessionID string
+	WithSession string
+
+	// WithRenew adds a renew interval to the lock; the lock will continuously
+	// re-acquire itself at this interval until it fails or is terminated.
+	WithRenew time.Duration
 )
 
 // Apply resolves the provided list of options.
@@ -47,8 +52,12 @@ func (o WithTimeout) request(opt *Options) {
 	opt.Timeout = time.Duration(o)
 }
 
-func (o WithSessionID) request(opt *Options) {
-	opt.SessionID = string(o)
+func (o WithSession) request(opt *Options) {
+	opt.Session = string(o)
+}
+
+func (o WithRenew) request(opt *Options) {
+	opt.Renew = time.Duration(o)
 }
 
 func (o *Options) del() *statestore.DelOptions {
