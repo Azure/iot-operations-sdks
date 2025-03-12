55c13c84b8e4da993a6baa9465fd137ac5951361
timtay-microsoft <timtay@microsoft.com>
2025-03-12 15:47:45 -0700

    order

--- dotnet/src/Azure.Iot.Operations.Protocol/RPC/CommandInvoker.cs
+++ dotnet/src/Azure.Iot.Operations.Protocol/RPC/CommandInvoker.cs
@@ -477,14 +477,14 @@ namespace Azure.Iot.Operations.Protocol.RPC
             Guid requestGuid = metadata?.CorrelationId ?? Guid.NewGuid();
 
             TimeSpan reifiedCommandTimeout = commandTimeout ?? DefaultCommandTimeout;
-            // Rounding up to the nearest second
-            reifiedCommandTimeout = TimeSpan.FromSeconds(Math.Ceiling(reifiedCommandTimeout.TotalSeconds));
-
 
             if (reifiedCommandTimeout < MinimumCommandTimeout)
             {
                 throw AkriMqttException.GetArgumentInvalidException("commandTimeout", nameof(commandTimeout), reifiedCommandTimeout, $"commandTimeout must be at least {MinimumCommandTimeout}");
             }
+
+            // Rounding up to the nearest second
+            reifiedCommandTimeout = TimeSpan.FromSeconds(Math.Ceiling(reifiedCommandTimeout.TotalSeconds));
 
             if (reifiedCommandTimeout.TotalSeconds > uint.MaxValue)
             {
