bf35d4faca333ec6b5fdf078f358c07791c687c8
timtay-microsoft <timtay@microsoft.com>
2025-03-17 11:04:46 -0700

    documentation

--- dotnet/src/Azure.Iot.Operations.Mqtt/OrderedAckMqttClientOptions.cs
+++ dotnet/src/Azure.Iot.Operations.Mqtt/OrderedAckMqttClientOptions.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace Azure.Iot.Operations.Mqtt
 {
+    /// <summary>
+    /// The options for an <see cref="OrderedAckMqttClient"/>.
+    /// </summary>
     public class OrderedAckMqttClientOptions
     {
+        /// <summary>
+        /// Sets whether or not to use AIO broker-specific features. By default, this is true.
+        /// </summary>
         public bool EnableAIOBrokerFeatures { get; set; } = true;
     }
 }
