0a5ccaddf915615d74fe05bcda3261f1302fab2f
timtay-microsoft <timtay@microsoft.com>
2025-03-17 10:57:14 -0700

    [.NET] Add metric category user property to connect packets
    - Allows opt out in client options
    - Sent by both the session client and the basic MQTT client

---
+++ dotnet/src/Azure.Iot.Operations.Mqtt/OrderedAckMqttClientOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Iot.Operations.Mqtt
+{
+    public class OrderedAckMqttClientOptions
+    {
+        public bool EnableAIOBrokerFeatures { get; set; } = true;
+    }
+}
