3e81cc56b6e85b2f4a1789e75f52197524055056
olivakar <oliva.tanusree@gmail.com>
2025-03-19 00:03:26 +0000

    codegen

---
+++ rust/azure_iot_operations_services/src/adr/gen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+../../../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net8.0/Azure.Iot.Operations.ProtocolCompiler \
+ --clientOnly --modelFile ../../../../eng/dtdl/aep-name-based-operations.json --sdkPath ../ --lang=rust --noProj \
+ --outDir src/adr/adr_name_gen
+
+ ../../../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net8.0/Azure.Iot.Operations.ProtocolCompiler \
+ --clientOnly --modelFile ../../../../eng/dtdl/aep-type-based-operations.json --sdkPath ../ --lang=rust --noProj \
+ --outDir src/adr/adr_type_gen
+ 
+cargo fmt
