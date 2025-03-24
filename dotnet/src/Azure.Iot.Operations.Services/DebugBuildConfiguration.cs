a52c9c44f503384aa1f95412c26e077a904d54b9
timtay-microsoft <timtay@microsoft.com>
2025-03-24 13:53:51 -0700

    fix?

---
+++ dotnet/src/Azure.Iot.Operations.Services/DebugBuildConfiguration.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.CompilerServices;
+
+#if (DEBUG)
+[assembly: InternalsVisibleTo("Azure.Iot.Operations.Services.UnitTests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+#endif
