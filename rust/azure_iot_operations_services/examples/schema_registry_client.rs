f0d711c6a189106e395d16c7db47e02316a46752
Carter Tinney <carter.tinney@microsoft.com>
2025-03-24 08:06:23 -0700

    sr fix

--- rust/azure_iot_operations_services/examples/schema_registry_client.rs
+++ rust/azure_iot_operations_services/examples/schema_registry_client.rs
@@ -106,7 +106,7 @@ async fn schema_registry_put(
         .await
     {
         Ok(schema) => {
-            log::info!("Put request succeeded: {schema}");
+            log::info!("Put request succeeded: {schema:?}");
             // Send the schema ID to the other task
             schema_id_tx.send(schema.name.unwrap()).unwrap();
         }
@@ -131,7 +131,7 @@ async fn schema_registry_get(
                 .await
             {
                 Ok(schema) => {
-                    log::info!("Got schema: {schema}");
+                    log::info!("Got schema: {schema:?}");
                 }
                 Err(e) => {
                     log::error!("Failed to get schema: {e}");
