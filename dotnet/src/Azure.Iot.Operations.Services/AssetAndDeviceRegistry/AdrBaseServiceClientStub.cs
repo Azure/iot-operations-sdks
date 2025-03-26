d8939e448384a5a8c3c4961c2075196a980204e5
Maxim Semenov <maximsemenov@microsoft.com>
2025-03-26 15:20:49 -0700

    progress

--- dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/AdrBaseServiceClientStub.cs
+++ dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/AdrBaseServiceClientStub.cs
@@ -10,22 +10,28 @@ namespace Azure.Iot.Operations.Services.AssetAndDeviceRegistry;
 internal class AdrBaseServiceClientStub(ApplicationContext applicationContext, IMqttPubSubClient mqttClient, Dictionary<string, string>? topicTokenMap = null)
     : AdrBaseService.AdrBaseService.Client(applicationContext, mqttClient, topicTokenMap)
 {
-    public event Func<string, AssetEndpointProfileUpdateEventTelemetry, IncomingTelemetryMetadata, Task>? OnReceiveAssetEndpointProfileUpdateTelemetry;
-    public event Func<string, AssetUpdateEventTelemetry, IncomingTelemetryMetadata, Task>? OnReceiveAssetUpdateEventTelemetry;
+    internal event Func<string, AssetEndpointProfile?, Task>? OnReceiveAssetEndpointProfileUpdateTelemetry;
+    internal event Func<string, Asset?, Task>? OnReceiveAssetUpdateEventTelemetry;
 
     public override async Task ReceiveTelemetry(string senderId, AssetEndpointProfileUpdateEventTelemetry telemetry, IncomingTelemetryMetadata metadata)
     {
+        var aepName = telemetry.AssetEndpointProfileUpdateEvent.AssetEndpointProfile?.Name ?? string.Empty;
+        AssetEndpointProfile? assetEndpointProfile = telemetry.AssetEndpointProfileUpdateEvent.AssetEndpointProfile;
+
         if (OnReceiveAssetEndpointProfileUpdateTelemetry != null)
         {
-            await OnReceiveAssetEndpointProfileUpdateTelemetry.Invoke(senderId, telemetry, metadata);
+            await OnReceiveAssetEndpointProfileUpdateTelemetry.Invoke(aepName, assetEndpointProfile);
         }
     }
 
     public override async Task ReceiveTelemetry(string senderId, AssetUpdateEventTelemetry telemetry, IncomingTelemetryMetadata metadata)
     {
+        string assetName = telemetry.AssetUpdateEvent.AssetName!;
+        Asset? asset = telemetry.AssetUpdateEvent.Asset;
+
         if (OnReceiveAssetUpdateEventTelemetry != null)
         {
-            await OnReceiveAssetUpdateEventTelemetry.Invoke(senderId, telemetry, metadata);
+            await OnReceiveAssetUpdateEventTelemetry.Invoke(assetName, asset);
         }
     }
 }
