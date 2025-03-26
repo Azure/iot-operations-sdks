7c7ff1521fc35d5bbc98fd7b4680764591d53580
Tim Taylor <timtay@microsoft.com>
2025-03-26 19:03:34 +0000

    asdf

--- dotnet/src/Azure.Iot.Operations.Services/Observability/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/Observability/gen.sh
@@ -2,7 +2,7 @@ rm -rf ./AkriObservabilityServiceMetricsApis
 mkdir ./AkriObservabilityServiceMetricsApis
 rm -rf ./Common
 mkdir ./Common
-../../../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net8.0/Azure.Iot.Operations.ProtocolCompiler --modelFile ../../../../eng/dtdl/akri-observability-metrics-operations.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.AkriObservabilityServiceMetricsApis
+../../../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --modelFile ../../../../eng/dtdl/akri-observability-metrics-operations.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.AkriObservabilityServiceMetricsApis
 cp -f /tmp/Azure.Iot.Operations.Services.AkriObservabilityServiceMetricsApis/AkriObservabilityServiceMetricsApis/*.cs AkriObservabilityServiceMetricsApis -v
 cp -f /tmp/Azure.Iot.Operations.Services.AkriObservabilityServiceMetricsApis/*.cs Common -v
 rm -rf /tmp/Azure.Iot.Operations.Services.AkriObservabilityServiceMetricsApis -v
