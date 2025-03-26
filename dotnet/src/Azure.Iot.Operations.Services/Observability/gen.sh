b8308be8e6d99e6f81eeebc2bef213eb77f0dfe0
timtay-microsoft <timtay@microsoft.com>
2025-03-26 11:09:19 -0700

    Update gen.sh

--- dotnet/src/Azure.Iot.Operations.Services/Observability/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/Observability/gen.sh
@@ -1,5 +1,5 @@
-rm -rf ./ObservabilityClient
-mkdir ./ObservabilityClient
+rm -rf ./AkriObservabilityServiceMetricsApis
+mkdir ./AkriObservabilityServiceMetricsApis
 rm -rf ./Common
 mkdir ./Common
 ../../../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net8.0/Azure.Iot.Operations.ProtocolCompiler --modelFile ../../../../eng/dtdl/akri-observability-metrics-operations.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.Observability
