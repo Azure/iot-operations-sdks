e7a257994db8340d6c92b31267178c0a7d98d703
Maxim Semenov <maximsemenov@microsoft.com>
2025-03-31 14:03:11 -0700

    address review comments

--- dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/IAdrServiceClient.cs
+++ dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/IAdrServiceClient.cs
@@ -8,30 +8,20 @@ namespace Azure.Iot.Operations.Services.AssetAndDeviceRegistry;
 
 public interface IAdrServiceClient : IAsyncDisposable
 {
-    Task<NotificationResponse> ObserveAssetEndpointProfileUpdatesAsync(string aepName,
-        Func<string, AssetEndpointProfile?, Task> onAssetEndpointProfileUpdateEventReceived,
-        CancellationToken cancellationToken);
+    Task<NotificationResponse> ObserveAssetEndpointProfileUpdatesAsync(string aepName, CancellationToken cancellationToken);
 
-    Task<NotificationResponse> UnobserveAssetEndpointProfileUpdatesAsync(string aepName,
-        CancellationToken cancellationToken);
+    Task<NotificationResponse> UnobserveAssetEndpointProfileUpdatesAsync(string aepName, CancellationToken cancellationToken);
 
-    Task<AssetEndpointProfile?> GetAssetEndpointProfileAsync(string aepName,
-        TimeSpan? commandTimeout = null,
-        CancellationToken cancellationToken = default);
+    Task<AssetEndpointProfile?> GetAssetEndpointProfileAsync(string aepName, TimeSpan? commandTimeout = null, CancellationToken cancellationToken = default);
 
     Task<AssetEndpointProfile?> UpdateAssetEndpointProfileStatusAsync(string aepName,
         UpdateAssetEndpointProfileStatusRequestPayload requestPayload,
         TimeSpan? commandTimeout = null,
         CancellationToken cancellationToken = default);
 
-    Task<NotificationResponse> ObserveAssetUpdatesAsync(string aepName,
-        string assetName,
-        Func<string, Asset?, Task> onAssetUpdateEventReceived,
-        CancellationToken cancellationToken);
+    Task<NotificationResponse> ObserveAssetUpdatesAsync(string aepName, string assetName, CancellationToken cancellationToken);
 
-    Task<NotificationResponse> UnobserveAssetUpdatesAsync(string aepName,
-        string assetName,
-        CancellationToken cancellationToken);
+    Task<NotificationResponse> UnobserveAssetUpdatesAsync(string aepName, string assetName, CancellationToken cancellationToken);
 
     Task<Asset?> GetAssetAsync(string aepName,
         GetAssetRequestPayload requestPayload,
@@ -52,4 +42,7 @@ public interface IAdrServiceClient : IAsyncDisposable
         CreateDiscoveredAssetEndpointProfileRequestPayload requestPayload,
         TimeSpan? commandTimeout = null,
         CancellationToken cancellationToken = default);
+
+    event Func<string, AssetEndpointProfile?, Task>? OnReceiveAssetEndpointProfileUpdateTelemetry;
+    event Func<string, Asset?, Task>? OnReceiveAssetUpdateEventTelemetry;
 }
