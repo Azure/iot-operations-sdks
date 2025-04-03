a792ca2c3358f008a7874fdb48f70cb59133b109
timtay-microsoft <timtay@microsoft.com>
2025-04-03 14:13:59 -0700

    test

--- dotnet/samples/Protocol/Codegen/SampleClient/Program.cs
+++ dotnet/samples/Protocol/Codegen/SampleClient/Program.cs
@@ -14,7 +14,7 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddTransient<MathClient>();
         services.AddTransient(CounterClient.Factory);
         services.AddTransient<MemMonClient>();
-        services.AddTransient<CustomTopicTokenClient>();
+        services.AddTransient<CustomTopicTokenClient>(); ;
     })
     .Build();
 
