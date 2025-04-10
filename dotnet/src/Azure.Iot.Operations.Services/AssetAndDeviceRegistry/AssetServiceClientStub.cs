6d25eb37c143c1737eca60be1259cb99c052ad18
Maxim Semenov <maximsemenov@microsoft.com>
2025-04-10 16:24:22 -0700

    progress

--- dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/AssetServiceClientStub.cs
+++ dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/AssetServiceClientStub.cs
@@ -11,18 +11,23 @@ namespace Azure.Iot.Operations.Services.AssetAndDeviceRegistry;
 internal class AssetServiceClientStub(ApplicationContext applicationContext, IMqttPubSubClient mqttClient, Dictionary<string, string>? topicTokenMap = null)
     : AdrBaseService.AdrBaseService.Client(applicationContext, mqttClient, topicTokenMap)
 {
-    internal event Func<string, Models.AssetEndpointProfile?, Task>? OnReceiveAssetEndpointProfileUpdateTelemetry;
+    internal event Func<string, Models.Device?, Task>? OnReceiveAssetEndpointProfileUpdateTelemetry;
     internal event Func<string, Models.Asset?, Task>? OnReceiveAssetUpdateEventTelemetry;
 
-    public override async Task ReceiveTelemetry(string senderId, AssetEndpointProfileUpdateEventTelemetry telemetry, IncomingTelemetryMetadata metadata)
-    {
-        var aepName = telemetry.AssetEndpointProfileUpdateEvent.AssetEndpointProfile?.Name ?? string.Empty;
-        Models.AssetEndpointProfile? assetEndpointProfile = telemetry.AssetEndpointProfileUpdateEvent.AssetEndpointProfile?.ToModel();
+    // public override async Task ReceiveTelemetry(string senderId, AssetEndpointProfileUpdateEventTelemetry telemetry, IncomingTelemetryMetadata metadata)
+    // {
+    //     var aepName = telemetry.AssetEndpointProfileUpdateEvent.AssetEndpointProfile?.Name ?? string.Empty;
+    //     Models.AssetEndpointProfile? assetEndpointProfile = telemetry.AssetEndpointProfileUpdateEvent.AssetEndpointProfile?.ToModel();
+    //
+    //     if (OnReceiveAssetEndpointProfileUpdateTelemetry != null)
+    //     {
+    //         await OnReceiveAssetEndpointProfileUpdateTelemetry.Invoke(aepName, assetEndpointProfile);
+    //     }
+    // }
 
-        if (OnReceiveAssetEndpointProfileUpdateTelemetry != null)
-        {
-            await OnReceiveAssetEndpointProfileUpdateTelemetry.Invoke(aepName, assetEndpointProfile);
-        }
+    public override async Task ReceiveTelemetry(string senderId, DeviceUpdateEventTelemetry telemetry, IncomingTelemetryMetadata metadata)
+    {
+        Models.Device? device = telemetry.DeviceUpdateEvent.Device?.ToModel();
     }
 
     public override async Task ReceiveTelemetry(string senderId, AssetUpdateEventTelemetry telemetry, IncomingTelemetryMetadata metadata)
---
+++ dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/Models/DeviceSpecification.cs
@@ -0,0 +1,59 @@
+﻿    // Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Azure.Iot.Operations.Services.AssetAndDeviceRegistry.Models;
+
+public record DeviceSpecification
+{
+        public Dictionary<string, string>? Attributes { get; set; } = default;
+
+        public string? DiscoveredDeviceRef { get; set; } = default;
+
+        public bool? Enabled { get; set; } = default;
+
+        public DeviceEndpoint? Endpoints { get; set; } = default;
+
+        public string? ExternalDeviceId { get; set; } = default;
+
+        public string? LastTransitionTime { get; set; } = default;
+
+        public string? Manufacturer { get; set; } = default;
+
+        public string? Model { get; set; } = default;
+
+        public string? OperatingSystemVersion { get; set; } = default;
+
+        public string? Uuid { get; set; } = default;
+
+        public ulong? Version { get; set; } = default;
+
+}
+
+public record DeviceEndpoint
+{
+    public Dictionary<string, DeviceInboundEndpointSchemaMapValue>? Inbound { get; set; } = default;
+}
+
+public record DeviceInboundEndpointSchemaMapValue
+{
+    public string? AdditionalConfiguration { get; set; } = default;
+
+    public string Address { get; set; } = default!;
+
+    public Authentication? Authentication { get; set; } = default;
+
+    public TrustSettings? TrustSettings { get; set; } = default;
+
+    public string Type { get; set; } = default!;
+
+    public string? Version { get; set; } = default;
+}
+
+public record TrustSettings
+{
+    public string? IssuerList { get; set; } = default;
+
+    public string? TrustList { get; set; } = default;
+
+    public string TrustMode { get; set; } = default!;
+}
