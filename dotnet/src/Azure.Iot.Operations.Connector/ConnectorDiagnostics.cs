0d9672c12d9b611fa4df2a05e8343279d797017c
timtay-microsoft <timtay@microsoft.com>
2025-04-14 11:48:39 -0700

    keep

---
+++ dotnet/src/Azure.Iot.Operations.Connector/ConnectorDiagnostics.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Text.Json.Serialization;
+
+namespace Azure.Iot.Operations.Connector
+{
+    public class ConnectorDiagnostics
+    {
+        [JsonPropertyName("logs")]
+        public required AioDiagnosticsLogs Logs { get; set; }
+    }
+}
