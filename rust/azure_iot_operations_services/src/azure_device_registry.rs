c3aa4eaf64c1e3e0314d2144548ef7e5b07c093f
olivakar <oliva.tanusree@gmail.com>
2025-04-16 23:57:04 +0000

    wip

--- rust/azure_iot_operations_services/src/azure_device_registry.rs
+++ rust/azure_iot_operations_services/src/azure_device_registry.rs
@@ -21,8 +21,8 @@ impl From<MessageSchemaReference> for adr_base_service_gen::MessageSchemaReferen
     fn from(value: MessageSchemaReference) -> Self {
         adr_base_service_gen::MessageSchemaReference {
             schema_name: value.message_schema_name,
-            schema_namespace: value.message_schema_namespace,
             schema_version: value.message_schema_version,
+            schema_registry_namespace: value.message_schema_namespace,
         }
     }
 }
