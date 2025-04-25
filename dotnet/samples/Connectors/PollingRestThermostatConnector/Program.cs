9205bbb6b5a51f39aed62559e7040e76df78a950
timtay-microsoft <timtay@microsoft.com>
2025-04-25 16:53:51 -0700

    Revert "logs, to revert"
    This reverts commit 5713bc043c9235997c4aa2be43e0d17182ea82c9.

--- dotnet/samples/Connectors/PollingRestThermostatConnector/Program.cs
+++ dotnet/samples/Connectors/PollingRestThermostatConnector/Program.cs
@@ -1,12 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Diagnostics;
 using Azure.Iot.Operations.Connector;
 using Azure.Iot.Operations.Connector.ConnectorConfigurations;
 using Azure.Iot.Operations.Protocol;
 using RestThermostatConnector;
-Trace.Listeners.Add(new ConsoleTraceListener());
 
 string connectorClientId = Environment.GetEnvironmentVariable(ConnectorFileMountSettings.ConnectorClientIdEnvVar) ?? "todo";
 
--- dotnet/src/Azure.Iot.Operations.Connector/Assets/AssetFileMonitor.cs
+++ dotnet/src/Azure.Iot.Operations.Connector/Assets/AssetFileMonitor.cs
@@ -87,7 +87,6 @@ namespace Azure.Iot.Operations.Connector.Assets
 
                         foreach (string addedAssetName in newAssetNames)
                         {
-                            Trace.TraceInformation("Found updated state asset with name {}", addedAssetName);
                             _lastKnownAssetNames[assetFileName].Add(addedAssetName);
                             AssetFileChanged?.Invoke(this, new(deviceName, inboundEndpointName, addedAssetName, AssetFileMonitorChangeType.Created));
                         }
@@ -108,7 +107,6 @@ namespace Azure.Iot.Operations.Connector.Assets
                 {
                     foreach (string currentAssetName in currentAssetNames)
                     {
-                        Trace.TraceInformation("Found initial state asset with name {}", currentAssetName);
                         AssetFileChanged?.Invoke(this, new(deviceName, inboundEndpointName, currentAssetName, AssetFileMonitorChangeType.Created));
                     }
                 }
