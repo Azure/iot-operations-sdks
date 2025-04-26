9e7582f85e175b912051ad25f3f8ac5f7f73598d
RLeclair <raulleclair@microsoft.com>
2025-04-25 18:46:49 -0700

    filemount regression

--- rust/azure_iot_operations_connector/src/filemount.rs
+++ rust/azure_iot_operations_connector/src/filemount.rs
@@ -1,11 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-//! Contains structs and functions for interacting with a file mount and modules for utilities that
-//! use the file mount.
+//! Utilities for extracting data from filemounts in an Akri deployment
 
 pub mod azure_device_registry;
-
-// TODO: Add the file mount structs and functions here.
-
 pub mod connector_config;
+
+// TODO: Add common artifact structs and helpers here once implementation is unified
