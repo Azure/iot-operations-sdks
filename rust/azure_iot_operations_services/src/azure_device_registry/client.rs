7f38d1f74c3cc9df0b388b015a66035ce71a5040
olivakar <oliva.tanusree@gmail.com>
2025-05-06 12:24:44 -0700

    Update rust/azure_iot_operations_services/src/azure_device_registry/client.rs
    Co-authored-by: Valerie Avva Lim <54871851+vaavva@users.noreply.github.com>

--- rust/azure_iot_operations_services/src/azure_device_registry/client.rs
+++ rust/azure_iot_operations_services/src/azure_device_registry/client.rs
@@ -988,9 +988,7 @@ where
         timeout: Duration,
     ) -> Result<(), Error> {
         if asset_name.trim().is_empty() {
-            return Err(
-                ErrorKind::ValidationError("Asset name must be present".to_string()).into(),
-            );
+           Err(Error(ErrorKind::ValidationError("asset_name".to_string()))
         }
 
         let payload = adr_name_gen::SetNotificationPreferenceForAssetUpdatesRequestPayload {
