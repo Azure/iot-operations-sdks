59f42dc9b4d59788c5a1cf1666173b2aee14b141
olivakar <oliva.tanusree@gmail.com>
2025-05-08 21:56:00 +0000

    1 test

---
+++ rust/azure_iot_operations_services/tests/azure_device_registry_client_tests.rs
@@ -0,0 +1,112 @@
+#![cfg(feature = "azure_device_registry")]
+
+use std::{env, time::Duration};
+
+use env_logger::Builder;
+
+use azure_iot_operations_mqtt::MqttConnectionSettingsBuilder;
+use azure_iot_operations_mqtt::session::{
+    Session, SessionExitHandle, SessionManagedClient, SessionOptionsBuilder,
+};
+use azure_iot_operations_protocol::application::ApplicationContextBuilder;
+use azure_iot_operations_services::azure_device_registry;
+
+const DEVICE1: &str = "my-thermostat";
+const DEVICE2: &str = "test-thermostat";
+const ENDPOINT1: &str = "my-rest-endpoint";
+const ASSET_NAME1: &str = "my-rest-thermostat-asset";
+const TIMEOUT: Duration = Duration::from_secs(10);
+
+// Test Scenarios:
+// get device
+// update status of device
+// observe device telemetry
+// unobserve device telemetry
+
+fn setup_test(
+    client_id: &str,
+) -> Result<
+    (
+        Session,
+        azure_device_registry::Client<SessionManagedClient>,
+        SessionExitHandle,
+    ),
+    (),
+> {
+    let _ = Builder::new()
+        .filter_level(log::LevelFilter::max())
+        .format_timestamp(None)
+        .filter_module("rumqttc", log::LevelFilter::Warn)
+        .filter_module("azure_iot_operations", log::LevelFilter::Warn)
+        .try_init();
+    if env::var("ENABLE_NETWORK_TESTS").is_err() {
+        log::warn!("This test is skipped. Set ENABLE_NETWORK_TESTS to run.");
+        return Err(());
+    }
+
+    let connection_settings = MqttConnectionSettingsBuilder::default()
+        .client_id(client_id)
+        .hostname("localhost")
+        .tcp_port(1883u16)
+        .keep_alive(Duration::from_secs(5))
+        .use_tls(false)
+        .build()
+        .unwrap();
+
+    let session_options = SessionOptionsBuilder::default()
+        .connection_settings(connection_settings)
+        .build()
+        .unwrap();
+
+    let session = Session::new(session_options).unwrap();
+    let application_context = ApplicationContextBuilder::default().build().unwrap();
+
+    let azure_device_registry_client = azure_device_registry::Client::new(
+        application_context,
+        session.create_managed_client(),
+        azure_device_registry::ClientOptionsBuilder::default()
+            .build()
+            .unwrap(),
+    )
+    .unwrap();
+    let exit_handle: SessionExitHandle = session.create_exit_handle();
+    Ok((session, azure_device_registry_client, exit_handle))
+}
+
+#[tokio::test]
+async fn get_device() {
+    let log_identifier = "get_device";
+    let Ok((session, azure_device_registry_client, exit_handle)) =
+        setup_test("get_device_network_tests-rust")
+    else {
+        // Network tests disabled, skipping tests
+        return;
+    };
+
+    let test_task = tokio::task::spawn({
+        async move {
+            let get_device_response = azure_device_registry_client
+                .get_device(DEVICE1.to_string(), ENDPOINT1.to_string(), TIMEOUT)
+                .await
+                .unwrap();
+            log::info!("[{log_identifier}] Get Device: {get_device_response:?}",);
+
+            assert_eq!(get_device_response.name, DEVICE1);
+
+            // Shutdown adr client and underlying resources
+            assert!(azure_device_registry_client.shutdown().await.is_ok());
+
+            exit_handle.try_exit().await.unwrap();
+        }
+    });
+
+    // if an assert fails in the test task, propagate the panic to end the test,
+    // while still running the test task and the session to completion on the happy path
+    assert!(
+        tokio::try_join!(
+            async move { test_task.await.map_err(|e| { e.to_string() }) },
+            async move { session.run().await.map_err(|e| { e.to_string() }) }
+        )
+        .is_ok()
+    );
+}
