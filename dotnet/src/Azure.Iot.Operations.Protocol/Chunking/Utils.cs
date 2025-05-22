b8c69cfda3203d87877030e2d45304689db97509
Maxim Semenov <maximsemenov@microsoft.com>
2025-05-21 21:21:53 -0700

    progress

---
+++ dotnet/src/Azure.Iot.Operations.Protocol/Chunking/Utils.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Azure.Iot.Operations.Protocol.Chunking;
+
+public static class Utils
+{
+    /// <summary>
+    /// Calculates the maximum size for a message chunk based on max packet size and overhead.
+    /// </summary>
+    /// <param name="maxPacketSize">The maximum packet size allowed by the broker.</param>
+    /// <param name="staticOverhead">The static overhead to account for in each chunk.</param>
+    /// <returns>The maximum size that can be used for a message chunk.</returns>
+    public static int GetMaxChunkSize(int maxPacketSize, int staticOverhead)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(maxPacketSize, staticOverhead);
+        return maxPacketSize - staticOverhead;
+    }
+}
