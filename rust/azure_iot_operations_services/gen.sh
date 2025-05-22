c705d4adec2f2e241680ac63c6a977c7afa427b7
Carter Tinney <carter.tinney@microsoft.com>
2025-05-22 11:39:11 -0700

    new shell script

--- rust/azure_iot_operations_services/gen.sh
+++ rust/azure_iot_operations_services/gen.sh
@@ -4,7 +4,7 @@
  --outDir src/schema_registry/schemaregistry_gen
 
 ../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
- --clientOnly --modelFile ../../eng/dtdl/device-name-based-operations.json --sdkPath ../ --lang=rust --noProj \
- --outDir src/azure_device_registry/device_name_gen
+ --clientOnly --modelFile ../../eng/dtdl/adr-base-service.json --sdkPath ../ --lang=rust --noProj \
+ --outDir src/azure_device_registry/adr_base_gen
 
 cargo fmt
