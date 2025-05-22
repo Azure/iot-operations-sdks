df59ee4f7321a4f5dcee3c837b9200037bad3e29
Maxim Semenov <maximsemenov@microsoft.com>
2025-05-21 21:47:06 -0700

    add integration tests

---
+++ dotnet/src/Azure.Iot.Operations.Protocol/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Azure.Iot.Operations.Protocol.IntegrationTests")]
+
+namespace Azure.Iot.Operations.Protocol;
+
+public class AssemblyInfo {
+    // This class is intentionally left empty.
+    // It serves as a placeholder for assembly-level attributes and metadata.
+}
