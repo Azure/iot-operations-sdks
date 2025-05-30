d7590fd481ba649668b53efddf3d82f04f123dd0
Carter Tinney <catinney@microsoft.com>
2025-05-30 09:24:59 -0700

    Update rust/azure_iot_operations_services/src/azure_device_registry/client.rs
    Co-authored-by: Valerie Avva Lim <54871851+vaavva@users.noreply.github.com>

--- rust/azure_iot_operations_services/src/azure_device_registry/client.rs
+++ rust/azure_iot_operations_services/src/azure_device_registry/client.rs
@@ -789,8 +789,8 @@ where
     /// Creates or updates a discovered device in the Azure Device Registry service.
     ///
     /// # Arguments
-    /// * `name` - The name of the discovered device.
-    /// * `specification` - The specification of the discovered device.
+    /// * `device_name` - The name of the discovered device.
+    /// * `device_specification` - The specification of the discovered device.
     /// * `inbound_endpoint_type` - The type of the inbound endpoint.
     /// * `timeout` - The duration until the client stops waiting for a response to the request, it is rounded up to the nearest second.
     ///
