811af2b95a573b5131a38d14a6a6346394100940
timtay-microsoft <timtay@microsoft.com>
2025-07-01 19:36:08 -0700

    Revert "asdf"
    This reverts commit 2842530101362c59ce9f81e7dbcfd6e34ff749a7.

--- dotnet/samples/Connectors/PollingRestThermostatConnector/Program.cs
+++ dotnet/samples/Connectors/PollingRestThermostatConnector/Program.cs
@@ -6,29 +6,17 @@ using Azure.Iot.Operations.Connector.ConnectorConfigurations;
 using Azure.Iot.Operations.Protocol;
 using RestThermostatConnector;
 
-while (true)
-{
-    try
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
     {
-        IHost host = Host.CreateDefaultBuilder(args)
-            .ConfigureServices(services =>
-            {
-                services.AddSingleton<ApplicationContext>();
-                services.AddSingleton(MqttSessionClientProvider.Factory);
-                services.AddSingleton(RestThermostatDatasetSamplerProvider.Factory);
-                services.AddSingleton(NoMessageSchemaProvider.Factory);
-                services.AddSingleton(LeaderElectionConfigurationProvider.Factory);
-                services.AddSingleton<IAdrClientWrapperProvider>(AdrClientWrapperProvider.Factory);
-                services.AddHostedService<PollingTelemetryConnectorWorker>();
-            })
-            .Build();
+        services.AddSingleton<ApplicationContext>();
+        services.AddSingleton(MqttSessionClientProvider.Factory);
+        services.AddSingleton(RestThermostatDatasetSamplerProvider.Factory);
+        services.AddSingleton(NoMessageSchemaProvider.Factory);
+        services.AddSingleton(LeaderElectionConfigurationProvider.Factory);
+        services.AddSingleton<IAdrClientWrapperProvider>(AdrClientWrapperProvider.Factory);
+        services.AddHostedService<PollingTelemetryConnectorWorker>();
+    })
+    .Build();
 
-        host.Run();
-
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-    }
-
-}
+host.Run();
