89af8349407daefca2032c9dff46e7728d65f4a6
timtay-microsoft <timtay@microsoft.com>
2025-07-08 11:34:55 -0700

    more

--- eng/test/schema-registry/src/Azure.Iot.Operations.Services.SchemaRegistry.Host/push.ps1
+++
@@ -1,4 +0,0 @@
-$v=(nbgv get-version -v NuGetPackageVersion)
-$registry="edgebuilds.azurecr.io"
-
-dotnet publish -c Release /t:PublishContainer  /p:ContainerImageTags="""$v;latest""" /p:ContainerRegistry="$registry"
