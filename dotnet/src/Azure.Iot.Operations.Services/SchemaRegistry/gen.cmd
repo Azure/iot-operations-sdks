eb62c2673ba93c7681e1147af268c42066bde074
timtay-microsoft <timtay@microsoft.com>
2025-07-08 11:40:59 -0700

    no SR

--- dotnet/src/Azure.Iot.Operations.Services/SchemaRegistry/gen.cmd
+++
@@ -1,4 +0,0 @@
-set codegen="..\..\..\..\..\tools\codegen\src\Azure.Iot.Operations.ProtocolCompiler\bin\Debug\net8.0\Azure.Iot.Operations.ProtocolCompiler.exe"
-%codegen% --modelFile ../../../../eng/dtdl/SchemaRegistry-1.json --lang csharp --outDir %TEMP%\Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry
-copy /y %TEMP%\Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry\SchemaRegistry\*.cs SchemaRegistry
-del /s /q %TEMP%\Azure.Iot.Operations.Services.SchemaRegistry.SchemaRegistry
