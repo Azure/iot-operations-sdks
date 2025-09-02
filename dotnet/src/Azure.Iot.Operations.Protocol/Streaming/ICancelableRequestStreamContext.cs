4dcd4b5044bdf26a9d289aa749b2300b5c1d6e63
timtay-microsoft <timtay@microsoft.com>
2025-09-02 11:58:36 -0700

    headers

--- dotnet/src/Azure.Iot.Operations.Protocol/Streaming/ICancelableRequestStreamContext.cs
+++ dotnet/src/Azure.Iot.Operations.Protocol/Streaming/ICancelableRequestStreamContext.cs
@@ -10,8 +10,20 @@ namespace Azure.Iot.Operations.Protocol.Streaming
     public interface ICancelableRequestStreamContext<T>
         where T : class
     {
+        /// <summary>
+        /// The asynchronously readable requests.
+        /// </summary>
         IAsyncEnumerable<StreamingExtendedRequest<T>> Requests { get; set; }
 
+        /// <summary>
+        /// Cancel this received RPC streaming request.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token for this cancellation request</param>
+        /// <remarks>
+        /// This method may be called by the streaming executor at any time. For instance, if the request stream
+        /// stalls unexpectedly, the executor can call this method to notify the invoker to stop sending requests.
+        /// Additionally, the executor can call this method if its response stream has stalled unexpectedly.
+        /// </remarks>
         Task CancelAsync(CancellationToken cancellationToken = default);
     }
 }
--- dotnet/src/Azure.Iot.Operations.Protocol/Streaming/ICancelableResponseStreamContext.cs
+++ dotnet/src/Azure.Iot.Operations.Protocol/Streaming/ICancelableResponseStreamContext.cs
@@ -10,8 +10,20 @@ namespace Azure.Iot.Operations.Protocol.Streaming
     public interface ICancelableResponseStreamContext<T>
         where T : class
     {
+        /// <summary>
+        /// The asynchronously readable responses.
+        /// </summary>
         IAsyncEnumerable<StreamingExtendedResponse<T>> Responses { get; set; }
 
+        /// <summary>
+        /// Cancel this RPC streaming request.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token for this cancellation request</param>
+        /// <remarks>This method may be called by the streaming invoker at any time. For instance, if the invoker's
+        /// request stream has stalled unexpectedly, the invoker can call this to notify the executor that
+        /// the RPC has been canceled. Additionally, if the stream of responses is taking longer than expected
+        /// or is no longer wanted by the invoker, the invoker may call this to notify the executor to stop sending
+        /// responses.</remarks>
         Task CancelAsync(CancellationToken cancellationToken = default);
     }
 }
