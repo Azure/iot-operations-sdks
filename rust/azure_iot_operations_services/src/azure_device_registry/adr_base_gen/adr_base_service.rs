7312f12e88898dd5e12f3350333f689b48e99215
vaavva <vaavva@microsoft.com>
2025-09-11 12:42:33 -0400

    update guess status updates in dtdl

--- rust/azure_iot_operations_services/src/azure_device_registry/adr_base_gen/adr_base_service.rs
+++ rust/azure_iot_operations_services/src/azure_device_registry/adr_base_gen/adr_base_service.rs
@@ -11,6 +11,7 @@ mod asset_dataset_schema_element_schema;
 mod asset_device_ref;
 mod asset_event_data_point_schema_element_schema;
 mod asset_event_group_schema_element_schema;
+mod asset_event_group_status_schema_element_schema;
 mod asset_event_schema_element_schema;
 mod asset_management_group_action_schema_element_schema;
 mod asset_management_group_action_status_schema_element_schema;
@@ -145,6 +146,7 @@ pub mod client {
     pub use super::asset_device_ref::*;
     pub use super::asset_event_data_point_schema_element_schema::*;
     pub use super::asset_event_group_schema_element_schema::*;
+    pub use super::asset_event_group_status_schema_element_schema::*;
     pub use super::asset_event_schema_element_schema::*;
     pub use super::asset_management_group_action_schema_element_schema::*;
     pub use super::asset_management_group_action_status_schema_element_schema::*;
