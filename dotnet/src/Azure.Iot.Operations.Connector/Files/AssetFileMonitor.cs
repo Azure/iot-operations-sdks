dcff0072aa94c5bb10d7ecb35700db321d0edef7
timtay-microsoft <timtay@microsoft.com>
2025-09-17 18:01:12 -0700

    todo
    TODO the file is getting updated, but connector not notified by ADR client?

--- dotnet/src/Azure.Iot.Operations.Connector/Files/AssetFileMonitor.cs
+++ dotnet/src/Azure.Iot.Operations.Connector/Files/AssetFileMonitor.cs
@@ -51,7 +51,7 @@ namespace Azure.Iot.Operations.Connector.Files
         /// </param>
         public AssetFileMonitor(IFilesMonitorFactory? filesMonitorFactory = null)
         {
-            _filesMonitorFactory = filesMonitorFactory ?? new FsnotifyFilesMonitorFactory();
+            _filesMonitorFactory = filesMonitorFactory ?? new PollingFilesMonitorFactory();
             _adrResourcesNameMountPath = Environment.GetEnvironmentVariable(AdrResourcesNameMountPathEnvVar) ?? throw new InvalidOperationException($"Missing {AdrResourcesNameMountPathEnvVar} environment variable");
             _deviceEndpointTlsTrustBundleCertMountPath = Environment.GetEnvironmentVariable(DeviceEndpointTlsTrustBundleCertMountPathEnvVar);
             _deviceEndpointCredentialsMountPath = Environment.GetEnvironmentVariable(DeviceEndpointCredentialsMountPathEnvVar);
