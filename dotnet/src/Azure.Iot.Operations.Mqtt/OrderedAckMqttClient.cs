1facff740b332032b6f14fb520615c87b1b15c4c
timtay-microsoft <timtay@microsoft.com>
2025-09-25 11:13:36 -0700

    to revert

--- dotnet/src/Azure.Iot.Operations.Mqtt/OrderedAckMqttClient.cs
+++ dotnet/src/Azure.Iot.Operations.Mqtt/OrderedAckMqttClient.cs
@@ -7,6 +7,7 @@ using Azure.Iot.Operations.Protocol.Models;
 using Azure.Iot.Operations.Mqtt.Converters;
 using System.Diagnostics;
 using Azure.Iot.Operations.Protocol;
+using System.Text;
 
 namespace Azure.Iot.Operations.Mqtt;
 
@@ -219,6 +220,13 @@ public class OrderedAckMqttClient : IMqttPubSubClient, IMqttClient
             {
                 try
                 {
+                    if (mqttNetArgs.ApplicationMessage.Topic.Contains("getAsset"))
+                    {
+                        Trace.TraceInformation("#####");
+                        Trace.TraceInformation(Encoding.UTF8.GetString(mqttNetArgs.ApplicationMessage.Payload));
+                        Trace.TraceInformation("#####");
+                    }
+
                     await ApplicationMessageReceivedAsync.Invoke(
                         MqttNetConverter.ToGeneric(
                             mqttNetArgs,
