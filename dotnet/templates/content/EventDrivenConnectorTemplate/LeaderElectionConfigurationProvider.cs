2ed2532fa99ffd8721f6c5cfd4a321c651607994
timtay-microsoft <timtay@microsoft.com>
2025-10-23 11:32:36 -0700

    More documentation

--- dotnet/templates/content/EventDrivenConnectorTemplate/LeaderElectionConfigurationProvider.cs
+++ dotnet/templates/content/EventDrivenConnectorTemplate/LeaderElectionConfigurationProvider.cs
@@ -5,6 +5,15 @@ using Azure.Iot.Operations.Connector;
 
 namespace EventDrivenTelemetryConnector
 {
+    /// <summary>
+    /// Factory that provides all leader election configurations for this connector.
+    /// </summary>
+    /// <remarks>
+    /// Connectors can have active/passive replication by deploying more than one replica and having each replica
+    /// provide the same leadership position Id. That way, only one replica is the leader at a time.
+    ///
+    /// If no replication of this connector is needed, then this class can be deleted.
+    /// </remarks>
     public class LeaderElectionConfigurationProvider : IConnectorLeaderElectionConfigurationProvider
     {
         public static Func<IServiceProvider, IConnectorLeaderElectionConfigurationProvider> Factory = service =>
@@ -14,7 +23,9 @@ namespace EventDrivenTelemetryConnector
 
         public ConnectorLeaderElectionConfiguration GetLeaderElectionConfiguration()
         {
-            return new("some-leadership-position-id", TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(9));
+            // This value should be the same across any replicas but should be unique from connector to connector.
+            string leadershipPositionId = "some-leadership-position-id";
+            return new(leadershipPositionId, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(9));
         }
     }
 }
