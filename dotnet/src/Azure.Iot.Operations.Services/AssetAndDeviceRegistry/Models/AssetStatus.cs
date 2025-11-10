2eaffb4e6e256efe398eaad65a9e817490d057c2
timtay-microsoft <timtay@microsoft.com>
2025-11-10 11:09:58 -0800

    documentation

--- dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/Models/AssetStatus.cs
+++ dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/Models/AssetStatus.cs
@@ -17,21 +17,32 @@ public record AssetStatus
     /// <summary>
     /// The status of all datasets associated with this asset (if it has any datasets).
     /// </summary>
+    /// <remarks>
+    /// Each dataset should only report its latest status.
+    /// </remarks>
     public List<AssetDatasetEventStreamStatus>? Datasets { get; set; }
 
     /// <summary>
     /// The status of all event groups associated with this asset (if it has any event groups).
     /// </summary>
+    /// <remarks>
+    /// Each event group should only report its latest status.
+    /// </remarks>
     public List<AssetEventGroupStatus>? EventGroups { get; set; } = default;
 
     /// <summary>
     /// The status of all management groups associated with this asset (if it has any management groups).
     /// </summary>
-
+    /// <remarks>
+    /// Each management group should only report its latest status.
+    /// </remarks>
     public List<AssetManagementGroupStatusSchemaElement>? ManagementGroups { get; set; }
 
     /// <summary>
     /// The status of all streams associated with this asset (if it has any streams).
     /// </summary>
+    /// <remarks>
+    /// Each stream should only report its latest status.
+    /// </remarks>
     public List<AssetDatasetEventStreamStatus>? Streams { get; set; }
 }
