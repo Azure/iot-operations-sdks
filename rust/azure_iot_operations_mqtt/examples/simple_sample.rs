7854cdf7d4f37d838bc9b85b29b29797d93730c1
Carter Tinney <catinney@microsoft.com>
2025-11-17 07:09:17 -0800

    reverted sample

--- rust/azure_iot_operations_mqtt/examples/simple_sample.rs
+++ rust/azure_iot_operations_mqtt/examples/simple_sample.rs
@@ -7,7 +7,9 @@ use env_logger::Builder;
 
 use azure_iot_operations_mqtt::MqttConnectionSettingsBuilder;
 use azure_iot_operations_mqtt::control_packet::QoS;
-use azure_iot_operations_mqtt::session::session::{Session, SessionOptionsBuilder, SessionManagedClient};
+use azure_iot_operations_mqtt::session::session::{
+    Session, SessionManagedClient, SessionOptionsBuilder,
+};
 
 const CLIENT_ID: &str = "aio_example_client";
 const HOSTNAME: &str = "localhost";
