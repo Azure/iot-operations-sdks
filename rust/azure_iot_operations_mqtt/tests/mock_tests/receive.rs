c7afd2629a963e7cf67bf637de61aed541c14807
Carter Tinney <catinney@microsoft.com>
2025-11-19 09:41:00 -0800

    wip

---
+++ rust/azure_iot_operations_mqtt/tests/mock_tests/receive.rs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+use azure_iot_operations_mqtt::MqttConnectionSettingsBuilder;
+use azure_iot_operations_mqtt::session::{Session, SessionOptionsBuilder};
+
+#[tokio::test]
+async fn test() {
+    
+}
