424215a2e4d01bb41728ea7d01bc4eaf8d04b5d4
Maxim Semenov <87454347+maximsemenov80@users.noreply.github.com>
2025-11-20 15:39:03 -0800

    Add unit tests for AioCloudEventBuilder

---
+++ dotnet/test/Azure.Iot.Operations.Connector.UnitTests/AioCloudEventBuilderTests.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Azure.Iot.Operations.Connector.UnitTests;
+
+public class AioCloudEventBuilderTests
+{
+    //TODO: implement tests for AioCloudEventBuilder
+}
