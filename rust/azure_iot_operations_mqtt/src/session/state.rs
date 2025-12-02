413073eaf0bea5bf936f36016731531dee7622cb
Carter Tinney <catinney@microsoft.com>
2025-12-02 06:55:06 -0800

    wip

--- rust/azure_iot_operations_mqtt/src/session/state.rs
+++ rust/azure_iot_operations_mqtt/src/session/state.rs
@@ -13,41 +13,29 @@ use tokio::sync::Notify;
 /// Information used to track the state of the Session.
 pub struct SessionState {
     /// State information locked for concurrency protection
-    state: RwLock<InnerSessionState>,
+    //state: RwLock<InnerSessionState>,
+    connected: RwLock<bool>,
     /// Notifier indicating a state change
     state_change: Notify,
 }
 
-/// The inner state containing the actual state data.
-struct InnerSessionState {
-    /// Indicates the part of the lifecycle the Session is currently in.
-    lifecycle_status: LifecycleStatus,
-    /// Indicates whether or not the Session is currently connected.
-    /// Note that this is best-effort information - it may not be accurate.
-    connected: bool,
-    /// Indicates if a Session exit is desired, and if so, by whom.
-    desire_exit: DesireExit,
-}
+// /// The inner state containing the actual state data.
+// struct InnerSessionState {
+//     /// Indicates the part of the lifecycle the Session is currently in.
+//     lifecycle_status: LifecycleStatus,
+//     /// Indicates whether or not the Session is currently connected.
+//     /// Note that this is best-effort information - it may not be accurate.
+//     connected: bool,
+//     /// Indicates if a Session exit is desired, and if so, by whom.
+//     desire_exit: DesireExit,
+// }
 
 // NOTE: There could be more methods implemented here, but they would not be used yet,
 // so they are omitted for now. Add as necessary.
 impl SessionState {
-    /// Return true if the Session has exited.
-    pub fn has_exited(&self) -> bool {
-        matches!(
-            self.state.read().unwrap().lifecycle_status,
-            LifecycleStatus::Exited
-        )
-    }
-
     /// Return true if the Session is currently connected (to the best of knowledge)
     pub fn is_connected(&self) -> bool {
-        self.state.read().unwrap().connected
-    }
-
-    /// Return true if the a Session exit is desired
-    pub fn desire_exit(&self) -> bool {
-        !matches!(self.state.read().unwrap().desire_exit, DesireExit::No)
+        *self.connected.read().unwrap()
     }
 
     /// Wait until the Session is connected.
@@ -73,40 +61,29 @@ impl SessionState {
         }
     }
 
-    /// Wait until the Session has exited.
-    /// Returns immediately if the Session has already exited.
-    pub async fn condition_exited(&self) {
-        loop {
-            if self.has_exited() {
-                break;
-            }
-            self.state_change.notified().await;
-        }
-    }
-
     /// Update the state to reflect a connection
     pub fn transition_connected(&self) {
         // Acquire write lock for duration of method to ensure correctness of logging
-        let mut state = self.state.write().unwrap();
+        let mut connected = self.connected.write().unwrap();
 
-        if state.connected {
-            // NOTE: I don't think this is possible, but just in case, log it.
+        if *connected {
+            // NOTE: This should never happen.
             log::warn!("Duplicate connection");
         } else {
-            state.connected = true;
+            *connected = true;
             log::info!("Connected!");
             self.state_change.notify_waiters();
         }
-        log::debug!("{state:?}");
+        log::debug!("{:?}", *connected);
     }
 
     /// Update the state to reflect a disconnection
     pub fn transition_disconnected(&self) {
         // Acquire write lock for duration of method to ensure correctness of logging
-        let mut state = self.state.write().unwrap();
+        let mut connected = self.connected.write().unwrap();
 
-        if state.connected {
-            state.connected = false;
+        if *connected {
+            *connected = false;
             match state.desire_exit {
                 DesireExit::No => log::info!("Connection lost."),
                 DesireExit::User => log::info!("Disconnected due to user-initiated Session exit"),
@@ -118,60 +95,14 @@ impl SessionState {
         }
         log::debug!("{state:?}");
     }
-
-    /// Update the state to reflect the Session is running
-    pub fn transition_running(&self) {
-        let mut state = self.state.write().unwrap();
-        state.lifecycle_status = LifecycleStatus::Running;
-        self.state_change.notify_waiters();
-        log::info!("Session started");
-        log::debug!("{state:?}");
-    }
-
-    /// Update the state to reflect the Session has exited
-    pub fn transition_exited(&self) {
-        let mut state = self.state.write().unwrap();
-        state.lifecycle_status = LifecycleStatus::Exited;
-        self.state_change.notify_waiters();
-        log::info!("Session exited");
-        log::debug!("{state:?}");
-    }
-
-    /// Update the state to reflect the user desires a Session exit
-    pub fn transition_user_desire_exit(&self) {
-        let mut state = self.state.write().unwrap();
-        state.desire_exit = DesireExit::User;
-        self.state_change.notify_waiters();
-        log::info!("User initiated Session exit process");
-        log::debug!("{state:?}");
-    }
-
-    /// Update the state to reflect the Session logic desires a Session exit
-    pub fn transition_session_desire_exit(&self) {
-        let mut state = self.state.write().unwrap();
-        state.desire_exit = DesireExit::SessionLogic;
-        self.state_change.notify_waiters();
-        log::info!("Session initiated Session exit process");
-        log::debug!("{state:?}");
-    }
 }
 
 impl Default for SessionState {
     /// Create a new `SessionState` with default values.
     fn default() -> Self {
         Self {
-            state: RwLock::new(InnerSessionState::default()),
+            connected: RwLock::new(false),
             state_change: Notify::new(),
-        }
-    }
-}
-
-impl Default for InnerSessionState {
-    fn default() -> Self {
-        Self {
-            lifecycle_status: LifecycleStatus::NotStarted,
-            connected: false,
-            desire_exit: DesireExit::No,
         }
     }
 }
