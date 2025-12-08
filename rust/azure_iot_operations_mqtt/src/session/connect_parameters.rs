0491f9542171b6bbe3dd52db0a6b44805a96c616
Carter Tinney <catinney@microsoft.com>
2025-12-08 09:33:20 -0800

    wip

--- rust/azure_iot_operations_mqtt/src/session/connect_parameters.rs
+++ rust/azure_iot_operations_mqtt/src/session/connect_parameters.rs
@@ -3,16 +3,24 @@
 
 use std::time::Duration;
 
+use azure_mqtt::client::ConnectionTransportConfig;
 use bytes::Bytes;
 
 use crate::control_packet::ConnectProperties;
 
 struct ConnectParameters {
-    initial_clean_start: bool,
-    keep_alive: azure_mqtt::client::KeepAliveConfig,
-    will: Option<azure_mqtt::packet::Will>,         // TODO: expose as re-export
-    username: Option<String>,
-    password: Option<Bytes>,
-    connect_properties: ConnectProperties,
-    connect_timeout: Duration,
+    pub initial_clean_start: bool,
+    pub keep_alive: azure_mqtt::client::KeepAliveConfig,
+    pub will: Option<azure_mqtt::packet::Will>,         // TODO: expose as re-export
+    pub username: Option<String>,
+    pub password: Option<Bytes>,
+    pub connect_properties: ConnectProperties,
+    pub connect_timeout: Duration,
+}
+
+impl ConnectParameters {
+    // // TODO: return a real error instead of String
+    // pub fn connection_transport_config(&self) -> Result<ConnectionTransportConfig, String> {
+
+    // }
 }
