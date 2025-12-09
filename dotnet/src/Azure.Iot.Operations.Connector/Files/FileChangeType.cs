0d3a566d786d9b7f64f334b65b0582992538f7e8
timtay-microsoft <timtay@microsoft.com>
2025-12-09 10:44:21 -0800

    revert

--- dotnet/src/Azure.Iot.Operations.Connector/Files/FileChangeType.cs
+++ dotnet/src/Azure.Iot.Operations.Connector/Files/FileChangeType.cs
@@ -1,12 +1,18 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Azure.Iot.Operations.Connector.Files
 {
     public enum FileChangeType
     {
         Deleted,
-        Updated,
-        Created
+        Created,
+        Updated
     }
 }
