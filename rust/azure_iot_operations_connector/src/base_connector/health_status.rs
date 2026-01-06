f9e6f4eea18d7eabb216bfb093fc5eb9d8340dd2
vaavva <vaavva@microsoft.com>
2026-01-06 17:55:43 -0500

    add functionality to be able to stop background interval status reporting

--- rust/azure_iot_operations_connector/src/base_connector/health_status.rs
+++ rust/azure_iot_operations_connector/src/base_connector/health_status.rs
@@ -26,7 +26,7 @@ pub(crate) trait HealthComponent: Clone + Send + Sync + 'static {
 pub(crate) fn new_health_sender<T: HealthComponent>(
     connector_context: Arc<ConnectorContext>,
     component: T,
-) -> UnboundedSender<RuntimeHealth> {
+) -> UnboundedSender<Option<RuntimeHealth>> {
     let (health_tx, health_rx) = tokio::sync::mpsc::unbounded_channel();
 
     // TODO: need cancellation token?
@@ -36,34 +36,15 @@ pub(crate) fn new_health_sender<T: HealthComponent>(
 // TODO: spawn this as a task in new? Or find some way to link this to baseConnector::run()
 async fn health_sender_run<T: HealthComponent>(
     connector_context: Arc<ConnectorContext>,
-    mut health_rx: UnboundedReceiver<RuntimeHealth>,
+    mut health_rx: UnboundedReceiver<Option<RuntimeHealth>>,
     component: T,
 ) {
-    let mut current_status = health_rx.recv().await.unwrap();
-    let mut last_reported_time = current_status.last_update_time;
+    let mut current_status = None;
+    let mut last_reported_time = None;
     loop {
-        // report current or new status
-        // TODO: tokio::task?
-        match component
-            .report_health_status(&connector_context, current_status.clone())
-            .await
-        {
-            Ok(_) => {
-                log::info!("Reported health status: {:?}", current_status);
-                // Setting to current time rather than current_status time in case the
-                // receiver is backed up - if we set to current_status time,
-                // the next report might trigger sooner than the health interval requires, causing the backup to worsen
-                last_reported_time = chrono::Utc::now();
-            }
-            Err(e) => {
-                // Handle error (e.g., log it)
-                log::warn!("Failed to report health status: {:?}", e);
-                // TODO: retry? Retries now done by not saving this as the last_reported_time - should this factor into the sleep time?
-            }
-        }
         tokio::select! {
             biased;
-            recv_result = health_recv(&mut health_rx, &mut current_status, last_reported_time.add(connector_context.health_report_interval)) => {
+            recv_result = health_recv(&mut health_rx, &mut current_status, last_reported_time.map(|t: DateTime<Utc>| t.add(connector_context.health_report_interval))) => {
                 match recv_result {
                     // TODO: need another way to end this task when the component is deleted as well
                     None => break, // Channel closed, TODO: handle this case properly
@@ -71,16 +52,42 @@ async fn health_sender_run<T: HealthComponent>(
                 }
             }
             _ = tokio::time::sleep(connector_context.health_report_interval) => {
-                current_status.last_update_time = chrono::Utc::now();
+                if let Some(curr_status) = &mut current_status {
+                    // update time to report steady state
+                    curr_status.last_update_time = chrono::Utc::now();
+                }
             }
+        }
+        // report current or new status
+        // TODO: tokio::task?
+        if let Some(ref curr_status) = current_status {
+            match component
+                .report_health_status(&connector_context, curr_status.clone())
+                .await
+            {
+                Ok(_) => {
+                    log::info!("Reported health status: {:?}", curr_status);
+                    // Setting to current time rather than current_status time in case the
+                    // receiver is backed up - if we set to current_status time,
+                    // the next report might trigger sooner than the health interval requires, causing the backup to worsen
+                    last_reported_time = Some(chrono::Utc::now());
+                }
+                Err(e) => {
+                    // Handle error (e.g., log it)
+                    log::warn!("Failed to report health status: {:?}", e);
+                    // TODO: retry? Retries now done by not saving this as the last_reported_time - should this factor into the sleep time?
+                }
+            }
+        } else {
+            last_reported_time = None;
         }
     }
 }
 async fn health_recv(
-    health_rx: &mut UnboundedReceiver<RuntimeHealth>,
-    curr_status: &mut RuntimeHealth,
-    next_fallback_report_time: DateTime<Utc>,
-) -> Option<RuntimeHealth> {
+    health_rx: &mut UnboundedReceiver<Option<RuntimeHealth>>,
+    curr_status: &mut Option<RuntimeHealth>,
+    next_fallback_report_time: Option<DateTime<Utc>>,
+) -> Option<Option<RuntimeHealth>> {
     loop {
         // use try_recv to avoid an await point if there are pending messages.
         let new_status = match health_rx.try_recv() {
@@ -89,28 +96,33 @@ async fn health_recv(
             Err(tokio::sync::mpsc::error::TryRecvError::Empty) => health_rx.recv().await?,
             Err(tokio::sync::mpsc::error::TryRecvError::Disconnected) => return None,
         };
+        let new_status = match new_status {
+            Some(status) => status,
+            None => return Some(None),
+        };
+        if let Some(existing_status) = curr_status {
+            // if new status is more stale than the current status, ignore it
+            if new_status.version < existing_status.version
+                || new_status.last_update_time < existing_status.last_update_time
+            {
+                continue;
+            }
 
-        // if new status is more stale than the current status, ignore it
-        if new_status.version < curr_status.version
-            || new_status.last_update_time < curr_status.last_update_time
-        {
-            continue;
+            // if status is exactly the same other than the timestamp, don't report, but update curr_status
+            if new_status.version == existing_status.version
+                && new_status.status == existing_status.status
+                && new_status.message == existing_status.message
+                && new_status.reason_code == existing_status.reason_code
+                // if we've been getting continuous reports that don't allow the the timeout branch of the tokio select to complete, trigger the
+                // steady state status reporting from here
+                && next_fallback_report_time.is_some_and(|next_fallback_report_time| new_status.last_update_time < next_fallback_report_time)
+            {
+                // Override the existing_status to have the latest timestamp
+                *existing_status = new_status;
+                continue;
+            }
         }
-
-        // if status is exactly the same other than the timestamp, don't report, but update curr_status
-        if new_status.version == curr_status.version
-            && new_status.status == curr_status.status
-            && new_status.message == curr_status.message
-            && new_status.reason_code == curr_status.reason_code
-            // if we've been getting continuous reports that don't allow the the timeout branch of the tokio select to complete, trigger the
-            // steady state status reporting from here
-            && new_status.last_update_time < next_fallback_report_time
-        {
-            // Override the curr_status to have the latest timestamp
-            *curr_status = new_status;
-            continue;
-        }
-        return Some(new_status);
+        return Some(Some(new_status));
     }
 }
 
