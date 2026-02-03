6cc23d7b561d7e7db49196c9617f6c38c8259197
Tim Taylor <timtay@microsoft.com>
2026-02-02 16:28:48 -0800

    just message

--- dotnet/samples/SampleRpcClient/SampleRpcClient/Program.cs
+++ dotnet/samples/SampleRpcClient/SampleRpcClient/Program.cs
@@ -117,7 +117,7 @@ internal class Program
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
+                //Console.WriteLine(e.StackTrace);
             }
         }
     }
