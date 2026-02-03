4ab4a33304db81b77a2b46840efe732f55d92e7d
Tim Taylor <timtay@microsoft.com>
2026-02-02 16:56:13 -0800

    Revert "just periodically send RPC?"
    This reverts commit 8a61236a098a69951732a9417c82aa480c732af4.

--- dotnet/samples/SampleRpcClient/SampleRpcClient/Program.cs
+++ dotnet/samples/SampleRpcClient/SampleRpcClient/Program.cs
@@ -26,77 +26,13 @@ internal class Program
 
         rpcInvoker = new(new(), mqttClient, "someCommandName", new Utf8JsonSerializer());
 
-        //mqttClient.ApplicationMessageReceivedAsync += MessageReceivedCallbackAsync;
+        mqttClient.ApplicationMessageReceivedAsync += MessageReceivedCallbackAsync;
 
-        //await mqttClient.SubscribeAsync(new MqttClientSubscribeOptions("timtay/topic"));
+        await mqttClient.SubscribeAsync(new MqttClientSubscribeOptions("timtay/topic"));
 
         Console.WriteLine("Waiting for MQTT publish to trigger mRPC call back to connector");
 
-        while (true)
-        {
-            await Task.Delay(1000);
-
-            try
-            {
-                //Console.WriteLine("ACK'ing the recieved MQTT publish from connector.");
-                //await args.AcknowledgeAsync(default);
-
-                Console.WriteLine("Recieved MQTT publish from connector. Sending mRPC back to connector");
-                var crm = new CommandRequestMetadata();
-                long stageThreeTicks = DateTime.UtcNow.Ticks;
-                crm.UserData.Add("stage3", stageThreeTicks + "");
-                var rpcResponse = await rpcInvoker!.InvokeCommandAsync(new PayloadObject(), crm);
-                Console.WriteLine("mRPC to connector returned.");
-                long stageFiveTicks = DateTime.UtcNow.Ticks; //note it repros at this commit (not this line in particular)
-
-
-                long? stageFourTicks = null;
-                if (rpcResponse.ResponseMetadata != null)
-                {
-                    foreach (string key in rpcResponse.ResponseMetadata.UserData.Keys)
-                    {
-                        if (key.Equals("stage4"))
-                        {
-                            stageFourTicks = long.Parse(rpcResponse.ResponseMetadata.UserData[key]);
-                        }
-                    }
-                }
-
-                long stageTwoTicks = DateTime.UtcNow.Ticks;
-
-                if (stageFourTicks == null)
-                {
-                    Console.WriteLine("Stage 4 ticks not found");
-                    return;
-                }
-
-                // Delay between TCP connector publishing message and this app receiving it from the broker
-                long delayOne = (stageThreeTicks - stageTwoTicks) / System.TimeSpan.TicksPerMillisecond;
-
-                // Delay between this app sending RPC and when the connector receiving it
-                long delayTwo = (stageFourTicks.Value - stageThreeTicks) / System.TimeSpan.TicksPerMillisecond;
-
-                // Delay between the connector sending mRPC response and this app receiving it
-                long delayThree = (stageFiveTicks - stageFourTicks.Value) / System.TimeSpan.TicksPerMillisecond;
-
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("MQTT pub from TCP connector -> MQTT app: " + delayOne);
-                Console.WriteLine("MQTT app invoke RPC -> connector receives invocation: " + delayTwo);
-                Console.WriteLine("connector sends RPC response -> MQTT app receives response: " + delayThree);
-                if (delayOne > 30 || delayTwo > 30 || delayThree > 30)
-                {
-                    Console.WriteLine("~~~~~~~~~~~~~Repro~~~~~~~~~~~~~");
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                //Console.WriteLine(e.StackTrace);
-            }
-        }
+        await Task.Delay(TimeSpan.FromHours(24));
     }
 
     private static Task MessageReceivedCallbackAsync(MqttApplicationMessageReceivedEventArgs args)
