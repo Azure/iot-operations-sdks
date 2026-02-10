c667cad42e36bc79f2d6eb3a0bf1610b6eb01cf9
Tim Taylor <timtay@microsoft.com>
2026-02-10 11:45:28 -0800

    telem

--- dotnet/samples/SimpleTelemetrySender/Program.cs
+++ dotnet/samples/SimpleTelemetrySender/Program.cs
@@ -4,6 +4,67 @@ internal class Program
 {
     private static async Task Main()
     {
-        
+        using var mqttClient1 = new MqttClientFactory().CreateMqttClient();
+        using var mqttClient2 = new MqttClientFactory().CreateMqttClient();
+        var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost", 1883).WithClientId(Guid.NewGuid().ToString()).Build();
+
+        await mqttClient1.ConnectAsync(mqttClientOptions);
+        await mqttClient2.ConnectAsync(mqttClientOptions);
+        Console.WriteLine("Connected");
+
+        TaskCompletionSource mqttClient1ReceivedMessage = new();
+        mqttClient1.ApplicationMessageReceivedAsync += async (args) =>
+        {
+            mqttClient1ReceivedMessage.TrySetResult();
+            args.AutoAcknowledge = true;
+        };
+
+        TaskCompletionSource mqttClient2ReceivedMessage = new();
+        mqttClient1.ApplicationMessageReceivedAsync += async (args) =>
+        {
+            mqttClient2ReceivedMessage.TrySetResult();
+            args.AutoAcknowledge = true;
+        };
+
+        await mqttClient1.SubscribeAsync(
+            new MqttClientSubscribeOptionsBuilder().WithTopicFilter(
+                new MqttTopicFilterBuilder()
+                .WithTopic("timtay/responseTopic")
+                .WithAtLeastOnceQoS()).Build());
+
+        await mqttClient2.SubscribeAsync(
+            new MqttClientSubscribeOptionsBuilder().WithTopicFilter(
+                new MqttTopicFilterBuilder()
+                .WithTopic("timtay/requestTopic")
+                .WithAtLeastOnceQoS()).Build());
+
+        MqttApplicationMessage msg1 =
+            new MqttApplicationMessageBuilder()
+                .WithTopic("timtay/requestTopic")
+                .WithResponseTopic("timtay/responseTopic")
+                .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+                .Build();
+
+        MqttApplicationMessage msg2 =
+            new MqttApplicationMessageBuilder()
+                .WithTopic("timtay/responseTopic")
+                .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+                .Build();
+
+        while (true)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            DateTime before = DateTime.UtcNow;
+            await mqttClient1.PublishAsync(msg1);
+            await mqttClient2ReceivedMessage.Task;
+            await mqttClient2.PublishAsync(msg2);
+            await mqttClient1ReceivedMessage.Task;
+            DateTime after = DateTime.UtcNow;
+            var diff = after.Subtract(before);
+            Console.WriteLine("DIFF! " + diff.TotalMilliseconds);
+            Console.WriteLine();
+        }
+
     }
 }
