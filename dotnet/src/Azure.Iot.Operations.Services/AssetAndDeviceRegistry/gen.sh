26399b743e4d924fabeec185dee56de5eef8ca80
Tim Taylor <timtay@microsoft.com>
2026-02-12 23:17:02 +0000

    split

--- dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/AssetAndDeviceRegistry/gen.sh
@@ -1,7 +1,8 @@
 rm -rf ./Generated
+rm -rf ./schemas
 
-dotnet run --project ../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/ --things ../../../../eng/wot/adr-base-service/AdrBaseService.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services.AssetAndDeviceRegistry --noProj --namespace Generated --common Generated.Common --typeNamer ../../../../eng/wot/adr-base-service/SchemaNames.json
-dotnet run --project ../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/ --things ../../../../eng/wot/device-discovery-service/DeviceDiscoveryService.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services.AssetAndDeviceRegistry --noProj --namespace Generated --common Generated.Common --typeNamer ../../../../eng/wot/device-discovery-service/SchemaNames.json
+dotnet run --project ../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/ --things ../../../../eng/wot/adr-base-service/AdrBaseService.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services.AssetAndDeviceRegistry --noProj --namespace Generated.AdrBaseService --common Generated.Common --typeNamer ../../../../eng/wot/adr-base-service/SchemaNames.json
+dotnet run --project ../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/ --things ../../../../eng/wot/device-discovery-service/DeviceDiscoveryService.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services.AssetAndDeviceRegistry --noProj --namespace Generated.DeviceDiscoveryService --common Generated.Common --typeNamer ../../../../eng/wot/device-discovery-service/SchemaNames.json
 
 mv -f ./Azure.Iot.Operations.Services.AssetAndDeviceRegistry/** .
 rm -rf ./Azure.Iot.Operations.Services.AssetAndDeviceRegistry
