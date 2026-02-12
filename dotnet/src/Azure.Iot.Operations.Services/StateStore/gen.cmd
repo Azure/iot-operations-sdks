e7fa8f401e8807bdf1eabacfbb8c6eaceea0c876
Tim Taylor <timtay@microsoft.com>
2026-02-12 11:02:32 -0800

    test

--- dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.cmd
+++
@@ -1,7 +0,0 @@
-set codegen="..\..\..\..\..\tools\codegen\src\Azure.Iot.Operations.ProtocolCompiler\bin\Debug\net8.0\Azure.Iot.Operations.ProtocolCompiler.exe"
-%codegen% --modelFile dss.json --lang csharp --outDir %TEMP%\Azure.Iot.Operations.Services.StateStore.Gen
-rmdir StateStoreGen /q /s
-mkdir StateStoreGen
-copy /y %TEMP%\Azure.Iot.Operations.Services.StateStore.Gen\StateStore\*.cs StateStoreGen\
-copy /y %TEMP%\Azure.Iot.Operations.Services.StateStore.Gen\PassthroughSerializer.cs StateStoreGen\PassthroughSerializer.cs
-del /s /q %TEMP%\Azure.Iot.Operations.Services.StateStore.Gen
--- dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
@@ -1,6 +1,2 @@
 rm -rf ./StateStoreGen
-mkdir ./StateStoreGen
-../../../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --modelFile ../../../../eng/dtdl/statestore.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.StateStore
-cp -f /tmp/Azure.Iot.Operations.Services.StateStore/StateStore/*.cs ./StateStoreGen -v
-cp -f /tmp/Azure.Iot.Operations.Services.StateStore/PassthroughSerializer.cs ./StateStoreGen -v
-rm -rf /tmp/Azure.Iot.Operations.Services.StateStore -v
+../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --things ../../../../eng/wot/statestore/StateStore.TM.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services --noProj --namespace GeneratedTypes --common GeneratedCommon --typeNamer ../../../../eng/wot/statestore/SchemaNames.json
