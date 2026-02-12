6a1b9f449e3d08e75302a3757aa8da5e48095c64
Tim Taylor <timtay@microsoft.com>
2026-02-12 14:17:39 -0800

    gen and move

--- dotnet/src/Azure.Iot.Operations.Services/SchemaRegistry/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/SchemaRegistry/gen.sh
@@ -1,1 +1,3 @@
-#../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --things ../../../../eng/wot/schemaregistry/SchemaRegistry.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services --noProj --namespace SchemaRegistry.Generated --common SchemaRegistry.Generated.Common --typeNamer ../../../../eng/wot/schemaregistry/SchemaNames.json
+dotnet run --project ../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/ --things ../../../../eng/wot/schemaregistry/SchemaRegistry.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services.SchemaRegistry --noProj --namespace Generated --common Generated.Common --typeNamer ../../../../eng/wot/schemaregistry/SchemaNames.json
+mv -f ./Azure.Iot.Operations.Services.SchemaRegistry/** .
+rm -rf ./Azure.Iot.Operations.Services.SchemaRegistry
--- dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
@@ -1,2 +1,3 @@
 dotnet run --project ../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/ --things ../../../../eng/wot/statestore/StateStore.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services --noProj --namespace StateStore.Generated --common StateStore.Generated.Common --typeNamer ../../../../eng/wot/statestore/SchemaNames.json
-
+mv -f ./Azure.Iot.Operations.Services.StateStore/** .
+rm -rf ./Azure.Iot.Operations.Services.StateStore
