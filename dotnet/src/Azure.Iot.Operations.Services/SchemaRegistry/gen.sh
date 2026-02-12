5ea01ecc364def66d6ce8c8907d065207f178ef4
Tim Taylor <timtay@microsoft.com>
2026-02-12 11:08:06 -0800

    fix dest

--- dotnet/src/Azure.Iot.Operations.Services/SchemaRegistry/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/SchemaRegistry/gen.sh
@@ -1,8 +1,3 @@
 rm -rf ./SchemaRegistry
-mkdir ./SchemaRegistry
 rm -rf ./Common
-mkdir ./Common
-../../../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --modelFile ../../../../eng/dtdl/SchemaRegistry-1.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services.SchemaRegistry
-cp -f /tmp/Azure.Iot.Operations.Services.SchemaRegistry/SchemaRegistry/*.cs SchemaRegistry -v
-cp -f /tmp/Azure.Iot.Operations.Services.SchemaRegistry/*.cs Common -v
-rm -rf /tmp/Azure.Iot.Operations.Services.SchemaRegistry -v
+../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --things ../../../../eng/wot/schemaregistry/SchemaRegistry.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services --noProj --namespace SchemaRegistryGenerated --common SchemaRegistryGeneratedCommon --typeNamer ../../../../eng/wot/schemaregistry/SchemaNames.json
--- dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
@@ -1,2 +1,2 @@
 rm -rf ./StateStoreGen
-../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --things ../../../../eng/wot/statestore/StateStore.TM.json --lang csharp --outDir /tmp/Azure.Iot.Operations.Services --noProj --namespace GeneratedTypes --common GeneratedCommon --typeNamer ../../../../eng/wot/statestore/SchemaNames.json
+../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --things ../../../../eng/wot/statestore/StateStore.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services --noProj --namespace StateStoreGenerated --common StateStoreGeneratedCommon --typeNamer ../../../../eng/wot/statestore/SchemaNames.json
