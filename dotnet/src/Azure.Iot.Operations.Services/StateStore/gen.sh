26d20a30ab7ca579eaa373a22db31081f6875de3
Tim Taylor <timtay@microsoft.com>
2026-02-12 11:35:46 -0800

    namespacing

--- dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
@@ -1,2 +1,2 @@
-rm -rf ./StateStoreGen
-../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --things ../../../../eng/wot/statestore/StateStore.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services --noProj --namespace StateStoreGenerated --common StateStoreGeneratedCommon --typeNamer ../../../../eng/wot/statestore/SchemaNames.json
+../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler --things ../../../../eng/wot/statestore/StateStore.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services --noProj --namespace StateStore.Generated --common StateStore.Generated.Common --typeNamer ../../../../eng/wot/statestore/SchemaNames.json
+
