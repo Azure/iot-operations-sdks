05a5167fab2224fdb86896a221649d9aa242aeb7
vaavva <vaavva@microsoft.com>
2026-02-18 14:12:44 -0500

    adr service updates

--- rust/azure_iot_operations_services/gen.sh
+++ rust/azure_iot_operations_services/gen.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 rm -r ./src/schema_registry/schemaregistry_gen
-../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
- --clientOnly --modelFile ../../eng/dtdl/SchemaRegistry-1.json --sdkPath ../ --lang=rust --noProj \
- --outDir src/schema_registry/schemaregistry_gen
+../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
+ --clientThings ../../eng/wot/schemaregistry/SchemaRegistry.TM.json --outDir src/schema_registry/schemaregistry_gen --lang rust \
+ --typeNamer ../../eng/wot/schemaregistry/SchemaNames.json \
+ --namespace schema_registry --workingDir target/akri/SchemaRegistry --sdkPath ../ --noProj
 
 rm -r ./src/azure_device_registry/adr_base_gen
-../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
- --clientOnly --modelFile ../../eng/dtdl/adr-base-service.json --sdkPath ../ --lang=rust --noProj \
- --outDir src/azure_device_registry/adr_base_gen
+../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
+ --clientThings ../../eng/wot/adr-base-service/AdrBaseService.TM.json --outDir src/azure_device_registry/adr_base_gen --lang rust \
+ --typeNamer ../../eng/wot/adr-base-service/SchemaNames.json \
+ --namespace adr_base_service --workingDir target/akri/AdrBaseService --sdkPath ../ --noProj
 
 rm -r ./src/azure_device_registry/device_discovery_gen
-../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
- --clientOnly --modelFile ../../eng/dtdl/device-discovery-service.json --sdkPath ../ --lang=rust --noProj \
- --outDir src/azure_device_registry/device_discovery_gen
+../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
+ --clientThings ../../eng/wot/device-discovery-service/DeviceDiscoveryService.TM.json --outDir src/azure_device_registry/device_discovery_gen --lang rust \
+ --typeNamer ../../eng/wot/device-discovery-service/SchemaNames.json \
+ --namespace device_discovery_service --workingDir target/akri/DeviceDiscoveryService --sdkPath ../ --noProj
 
 cargo fmt
