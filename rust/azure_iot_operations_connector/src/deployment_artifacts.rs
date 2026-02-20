92a17d5d40f326e8b6568d7d006b0ba704798751
Carter Tinney <catinney@microsoft.com>
2026-02-20 11:33:28 -0800

    revert

--- rust/azure_iot_operations_connector/src/deployment_artifacts.rs
+++ rust/azure_iot_operations_connector/src/deployment_artifacts.rs
@@ -6,7 +6,6 @@
 pub mod azure_device_registry;
 pub mod connector;
 mod filemount;
-mod secrets;
 
 pub use connector::DeploymentArtifactError; // TODO: move implementation out here
 pub use filemount::FileMount;
