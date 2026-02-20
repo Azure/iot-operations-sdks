19f48d548e1fa9f90272125709ed7c7df8f2e30a
Carter Tinney <catinney@microsoft.com>
2026-02-20 13:13:48 -0800

    [Rust] FileMount monitoring infrastructure (#1243)
    * Added a FileMount struct that can monitor for changes to a directory while providing main functionality of PathBuf
    * Updated ConnectorArtifacts to use this FileMount instead of PathBuf
    * Centralized testing utilities for filesystems

--- rust/azure_iot_operations_connector/src/base_connector/managed_azure_device_registry.rs
+++ rust/azure_iot_operations_connector/src/base_connector/managed_azure_device_registry.rs
@@ -28,7 +28,7 @@ use crate::{
     ManagementActionRef, MessageSchema, MessageSchemaReference,
     base_connector::ConnectorContext,
     deployment_artifacts::{
-        self,
+        self, FileMount,
         azure_device_registry::{AssetRef, DeviceEndpointRef},
     },
     destination_endpoint::{self, DataOperationForwarder},
@@ -4657,7 +4657,7 @@ pub(crate) enum DeviceSpecificationError {
 impl DeviceSpecification {
     pub(crate) fn new(
         device_specification: adr_models::Device,
-        device_endpoint_credentials_mount_path: Option<&PathBuf>,
+        device_endpoint_credentials_mount_path: Option<&FileMount>,
         inbound_endpoint_name: &str,
     ) -> Result<Self, DeviceSpecificationError> {
         // convert the endpoints to the new format with only the one specified inbound endpoint
