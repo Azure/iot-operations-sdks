6344b1c790dfbad8abc0be0bf45d9e3cc66f5a13
John (JD) Douceur <johndo@microsoft.com>
2026-02-24 15:46:59 -0800

    lint allow codegen code

--- rust/azure_iot_operations_facilities/src/lib.rs
+++ rust/azure_iot_operations_facilities/src/lib.rs
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+#![allow(clippy::approx_constant)]
+#![allow(clippy::unreadable_literal)]
+
 pub mod unit_converter;
 
 pub use unit_converter::{ConvertError, UnitConverter, get_converter};
