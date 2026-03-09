ffcc64d6e32d6b51f4a118301676d897a4a2ac74
Tim Taylor <timtay@microsoft.com>
2026-03-09 10:53:35 -0700

    asdf

--- dotnet/src/Azure.Iot.Operations.Connector/DeviceEndpointClient.cs
+++ dotnet/src/Azure.Iot.Operations.Connector/DeviceEndpointClient.cs
@@ -84,9 +84,10 @@ namespace Azure.Iot.Operations.Connector
             TimeSpan? commandTimeout = null,
             CancellationToken cancellationToken = default)
         {
-            string builder = "";
+            string builder = "Empty";
             if (status.Endpoints != null && status.Endpoints.Inbound != null)
             {
+                builder = "";
                 foreach (var endpoint in status.Endpoints.Inbound.Keys)
                 {
                     builder += endpoint + ";";
