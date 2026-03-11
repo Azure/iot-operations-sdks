249356d88cbbdba5cf4e799e28ee570895ee5275
John (JD) Douceur <johndo@microsoft.com>
2026-03-11 05:39:39 -0700

    rename folder codegen2 to wot-codegen

--- dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
+++ dotnet/src/Azure.Iot.Operations.Services/StateStore/gen.sh
@@ -1,4 +1,4 @@
 rm -rf ./Generated
-dotnet run --project ../../../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/ --things ../../../../eng/wot/statestore/StateStore.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services.StateStore --noProj --namespace Generated --common Generated.Common --typeNamer ../../../../eng/wot/statestore/SchemaNames.json
+dotnet run --project ../../../../wot-codegen/src/Azure.Iot.Operations.ProtocolCompiler/ --things ../../../../eng/wot/statestore/StateStore.TM.json --lang csharp --outDir ./Azure.Iot.Operations.Services.StateStore --noProj --namespace Generated --common Generated.Common --typeNamer ../../../../eng/wot/statestore/SchemaNames.json
 mv -f ./Azure.Iot.Operations.Services.StateStore/** .
 rm -rf ./Azure.Iot.Operations.Services.StateStore
--- rust/azure_iot_operations_services/gen.sh
+++ rust/azure_iot_operations_services/gen.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 rm -r ./src/schema_registry/schemaregistry_gen
-../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
+../../wot-codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
  --clientThings ../../eng/wot/schemaregistry/SchemaRegistry.TM.json --outDir src/schema_registry/schemaregistry_gen --lang rust \
  --namespace schema_registry --workingDir target/akri/SchemaRegistry --sdkPath ../ --noProj
 
 rm -r ./src/azure_device_registry/adr_base_gen
-../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
+../../wot-codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
  --clientThings ../../eng/wot/adr-base-service/AdrBaseServiceClientSide.TM.json --serverThings ../../eng/wot/adr-base-service/AdrBaseServiceServerSide.TM.json --outDir src/azure_device_registry/adr_base_gen --lang rust \
  --namespace adr_base_service --workingDir target/akri/AdrBaseService --sdkPath ../ --noProj
 
 rm -r ./src/azure_device_registry/device_discovery_gen
-../../codegen2/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
+../../wot-codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
  --clientThings ../../eng/wot/device-discovery-service/DeviceDiscoveryService.TM.json --outDir src/azure_device_registry/device_discovery_gen --lang rust \
  --namespace device_discovery_service --workingDir target/akri/DeviceDiscoveryService --sdkPath ../ --noProj
 
