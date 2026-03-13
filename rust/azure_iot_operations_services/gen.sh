80d39ddada07f8eedd0435753f97c87c8f4992bf
vaavva <vaavva@microsoft.com>
2026-03-13 13:26:44 -0500

    adding robustness from copilot comments

--- rust/azure_iot_operations_services/gen.sh
+++ rust/azure_iot_operations_services/gen.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-rm -r ./src/schema_registry/schemaregistry_gen
+set -e
+rm -rf ./src/schema_registry/schemaregistry_gen
 dotnet run --project ../../wot-codegen/src/Azure.Iot.Operations.ProtocolCompiler/ \
  --clientThings ../../eng/wot/schemaregistry/SchemaRegistry.TM.json --outDir src/schema_registry/schemaregistry_gen --lang rust \
  --namespace schema_registry --workingDir target/akri/SchemaRegistry --sdkPath ../ --noProj
 
-rm -r ./src/azure_device_registry/adr_base_gen
+rm -rf ./src/azure_device_registry/adr_base_gen
 dotnet run --project ../../wot-codegen/src/Azure.Iot.Operations.ProtocolCompiler/ \
  --clientThings ../../eng/wot/adr-base-service/AdrBaseServiceClientSide.TM.json --serverThings ../../eng/wot/adr-base-service/AdrBaseServiceServerSide.TM.json --outDir src/azure_device_registry/adr_base_gen --lang rust \
  --namespace adr_base_service --workingDir target/akri/AdrBaseService --sdkPath ../ --noProj
 
-rm -r ./src/azure_device_registry/device_discovery_gen
+rm -rf ./src/azure_device_registry/device_discovery_gen
 dotnet run --project ../../wot-codegen/src/Azure.Iot.Operations.ProtocolCompiler/ \
  --clientThings ../../eng/wot/device-discovery-service/DeviceDiscoveryService.TM.json --outDir src/azure_device_registry/device_discovery_gen --lang rust \
  --namespace device_discovery_service --workingDir target/akri/DeviceDiscoveryService --sdkPath ../ --noProj
