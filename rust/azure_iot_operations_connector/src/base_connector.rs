4b76527232ab738d44d3fda4cb81205a502bbf29
Carter Tinney <catinney@microsoft.com>
2026-03-26 15:58:55 -0700

    fix;r

--- rust/azure_iot_operations_connector/src/base_connector.rs
+++ rust/azure_iot_operations_connector/src/base_connector.rs
@@ -6,9 +6,8 @@
 use std::{sync::Arc, time::Duration};
 
 use azure_iot_operations_mqtt::session::{
-    SessionError, SessionExitErrorKind, SessionManagedClient,
-    reconnect_policy::{ExponentialBackoffWithJitter, ReconnectPolicy},
-    {Session, SessionOptionsBuilder},
+    SessionError, SessionExitErrorKind, SessionManagedClient, {Session, SessionOptionsBuilder},
+    reconnect_policy::{ReconnectPolicy, ExponentialBackoffWithJitter}
 };
 use azure_iot_operations_protocol::application::ApplicationContext;
 use azure_iot_operations_services::{
