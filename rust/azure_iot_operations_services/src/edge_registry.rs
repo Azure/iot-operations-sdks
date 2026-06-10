468522a72a07254d5ff0ff4351a6aff78baa8dae
RLeclair <raulleclair@microsoft.com>
2026-06-10 13:36:03 -0700

    [Services] Modify Edge Registry WoTs and add generated Rust code (#1352)

---
+++ rust/azure_iot_operations_services/src/edge_registry.rs
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+//! Types for Edge Registry (xRegistry) operations.
+
+/// Edge Registry generated code
+mod edge_registry_gen;
