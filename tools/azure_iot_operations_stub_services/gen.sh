9ab8b3fdd9cb5724ea1d4a4a380727d7053cbe38
John (JD) Douceur <johndo@microsoft.com>
2026-06-11 09:14:11 -0700

    revert changes to non-wot scripts

--- tools/azure_iot_operations_stub_services/gen.sh
+++ tools/azure_iot_operations_stub_services/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net10.0/Azure.Iot.Operations.ProtocolCompiler \
+../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
  --serverOnly --modelFile ../../eng/dtdl/SchemaRegistry-1.json --lang=rust --noProj \
  --outDir src/schema_registry/schema_registry_gen
  
