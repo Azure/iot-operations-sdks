4bdc11ad6a9172a042d1530d4d8074bbeecc6c53
Raul Leclair <raulleclair@microsoft.com>
2026-06-16 09:19:54 -0700

    [Services]  Adding generic xRegistry client support (#1361)
    This PR adds the Edge Registry feature, models, and generic xRegistry
    client wrapper
    
    Tests and example will come in a separate PR.

---
+++ rust/azure_iot_operations_services/src/edge_registry/models.rs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+//! Models for Edge Registry operations.
+
+pub mod xregistry;
+
+pub use xregistry::*;
+
+// TODO: Add the extensions here as well.
