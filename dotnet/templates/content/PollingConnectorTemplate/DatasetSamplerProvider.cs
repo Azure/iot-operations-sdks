069d0eb60c583b0288c4507b1c5d4d05b4f820cf
Tim Taylor <timtay@microsoft.com>
2026-07-30 11:56:53 -0700

    [.NET] Productize the templates projects (#1053)
    This follows the instructions laid out
    [here](https://learn.microsoft.com/en-us/dotnet/core/tutorials/cli-templates-create-template-package)
    and the example templates provided
    [here](https://github.com/dotnet/templating/tree/main/dotnet-template-samples).
    
    This is mostly just re-organizing the folder structure of the templates,
    but does include some changes to the csproj files involved

--- dotnet/templates/content/PollingConnectorTemplate/DatasetSamplerProvider.cs
+++ dotnet/templates/content/PollingConnectorTemplate/DatasetSamplerProvider.cs
@@ -7,6 +7,9 @@ using Azure.Iot.Operations.Services.AssetAndDeviceRegistry.Models;
 
 namespace PollingTelemetryConnectorTemplate
 {
+    /// <summary>
+    /// The factory for creating the samplers for each dataset.
+    /// </summary>
     public class DatasetSamplerProvider : IDatasetSamplerFactory
     {
         public static Func<IServiceProvider, IDatasetSamplerFactory> Factory = service =>
@@ -16,8 +19,9 @@ namespace PollingTelemetryConnectorTemplate
 
         public IDatasetSampler CreateDatasetSampler(string deviceName, Device device, string inboundEndpointName, string assetName, Asset asset, AssetDataset dataset, EndpointCredentials? endpointCredentials)
         {
-            // this method should return the appropriate dataset sampler implementation for the provided asset + dataset. This
-            // method may be called multiple times if the asset or dataset changes in any way over time.
+            // As the connector discovers each dataset, it will need to know how to sample that dataset. To figure that
+            // out, this callback is invoked to provide context on which dataset was discovered and what device + endpoint + asset it
+            // belongs to. This callback may be called multiple times if the asset or dataset changes in any way over time.
             throw new NotImplementedException();
         }
     }
--- dotnet/templates/content/PollingConnectorTemplate/Program.cs
+++ dotnet/templates/content/PollingConnectorTemplate/Program.cs
@@ -2,11 +2,8 @@
 // Licensed under the MIT License.
 
 using Azure.Iot.Operations.Connector;
-using Azure.Iot.Operations.Connector.ConnectorConfigurations;
 using Azure.Iot.Operations.Protocol;
 using PollingTelemetryConnectorTemplate;
-
-string connectorClientId = Environment.GetEnvironmentVariable(ConnectorFileMountSettings.ConnectorClientIdEnvVar) ?? throw new InvalidOperationException("No MQTT client Id configured by Akri operator");
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
