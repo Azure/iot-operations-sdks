af771e3686d015214fdf7adbad6364ec6c0f5f95
Tim Taylor <timtay@microsoft.com>
2026-07-31 17:00:41 -0700

    Unused

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-#Should be somewhere in here?
+echo "Starting postStartCommand"
 
-#echo "Starting postStartCommand"
-
-#echo "Environment variables:
-#    SUBSCRIPTION_ID:           $SUBSCRIPTION_ID
-#    RESOURCE_GROUP:            $RESOURCE_GROUP
-#    LOCATION:                  $LOCATION
-#    CLUSTER_NAME:              $CLUSTER_NAME
-#    STORAGE_ACCOUNT:           $STORAGE_ACCOUNT
-#    SCHEMA_REGISTRY:           $SCHEMA_REGISTRY
-#    SCHEMA_REGISTRY_NAMESPACE: $SCHEMA_REGISTRY_NAMESPACE"
+echo "Environment variables:
+    SUBSCRIPTION_ID:           $SUBSCRIPTION_ID
+    RESOURCE_GROUP:            $RESOURCE_GROUP
+    LOCATION:                  $LOCATION
+    CLUSTER_NAME:              $CLUSTER_NAME
+    STORAGE_ACCOUNT:           $STORAGE_ACCOUNT
+    SCHEMA_REGISTRY:           $SCHEMA_REGISTRY
+    SCHEMA_REGISTRY_NAMESPACE: $SCHEMA_REGISTRY_NAMESPACE"
 
 # Add a convenience alias for the aio-broker
-#sudo sh -c 'echo 127.0.0.1 aio-broker >> /etc/hosts'
+sudo sh -c 'echo 127.0.0.1 aio-broker >> /etc/hosts'
 
 # Stop and start the cluster, so its in a fresh state
-#k3d cluster stop
-#k3d cluster start --wait
+k3d cluster stop
+k3d cluster start --wait
 
-#echo "Ending postStartCommand"
+echo "Ending postStartCommand"
