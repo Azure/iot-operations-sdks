da477e0df9701bbf1b6790e70dfeb9ba54a50223
Tim Taylor <timtay@microsoft.com>
2026-08-05 10:41:03 -0700

    [Codespaces] Remove postStartCommand, move its logic to postCreateCommand (#1452)
    I'm seeing an odd bug whenever the postStartCommand is configured to run
    in codespaces where it overwrites our k8s client cert with an already
    expired version. This bug seems to have no relation to the logic within
    postStartCommand.sh, so we can just move it to postCreateCommand.sh
    
    before:
    <img width="1067" height="215" alt="image"
    src="https://github.com/user-attachments/assets/74ec8b78-29b9-4aad-9f3f-5d9c2e195fd4"
    />
    
    
    after:
    <img width="1075" height="217" alt="image"
    src="https://github.com/user-attachments/assets/058242f5-8296-4028-a82f-b3cb17f3d260"
    />

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -31,4 +31,20 @@ fi
 # Set the workspace as a safe directory
 git config --global --add safe.directory /workspaces
 
+echo "Environment variables:
+    SUBSCRIPTION_ID:           $SUBSCRIPTION_ID
+    RESOURCE_GROUP:            $RESOURCE_GROUP
+    LOCATION:                  $LOCATION
+    CLUSTER_NAME:              $CLUSTER_NAME
+    STORAGE_ACCOUNT:           $STORAGE_ACCOUNT
+    SCHEMA_REGISTRY:           $SCHEMA_REGISTRY
+    SCHEMA_REGISTRY_NAMESPACE: $SCHEMA_REGISTRY_NAMESPACE"
+
+# Add a convenience alias for the aio-broker
+sudo sh -c 'echo 127.0.0.1 aio-broker >> /etc/hosts'
+
+# Stop and start the cluster, so its in a fresh state
+k3d cluster stop
+k3d cluster start --wait
+
 echo "Ending postCreateCommand"
