164be78f93752d882979aa1934caf7920cc3c4e5
maximsemenov <maximsemenov@microsoft.com>
2026-08-28 13:46:03 -0700

    [.NET][MQTT][Connector] Allow AIO metriccategory metadata to be configured via API
    Mirrors Rust #1466. The metriccategory CONNECT user property is now taken from OrderedAckMqttClientOptions.MetricCategory (default aiosdk-dotnet) instead of being hardcoded, and the connector's session client reports aiosdk-dotnet-connector.

--- dotnet/src/Azure.Iot.Operations.Mqtt/OrderedAckMqttClient.cs
+++ dotnet/src/Azure.Iot.Operations.Mqtt/OrderedAckMqttClient.cs
@@ -87,7 +87,7 @@ public class OrderedAckMqttClient : IMqttPubSubClient, IMqttClient
 
         if (_clientOptions.EnableAIOBrokerFeatures)
         {
-            mqttNetOptions.UserProperties.Add(new("metriccategory", System.Text.Encoding.UTF8.GetBytes("aiosdk-dotnet")));
+            mqttNetOptions.UserProperties.Add(new("metriccategory", System.Text.Encoding.UTF8.GetBytes(_clientOptions.MetricCategory)));
         }
 
         MqttClientConnectResult? result =  MqttNetConverter.ToGeneric(await UnderlyingMqttClient.ConnectAsync(mqttNetOptions, cancellationToken).ConfigureAwait(false));
