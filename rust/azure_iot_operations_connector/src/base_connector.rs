b22c4366de0e7de48443a5993be30cff95e2540f
Carter Tinney <catinney@microsoft.com>
2026-08-28 09:35:22 -0700

    doc update

--- rust/azure_iot_operations_connector/src/base_connector.rs
+++ rust/azure_iot_operations_connector/src/base_connector.rs
@@ -105,7 +105,9 @@ pub struct Options {
     #[builder(default = "Duration::from_secs(5)")]
     filemount_debounce_duration: Duration,
 
-    /// Value for the `metriccategory` MQTT CONNECT user property
+    /// Value for the `metriccategory` that is used to categorize the internal traffic between 
+    /// the connector and other AIO services - Broker, Schema Registry, DSS. 
+    /// See https://learn.microsoft.com/en-us/azure/iot-operations/reference/observability-metrics-mqtt-broker#category for details.
     #[builder(default = "\"aiosdk-rust\".to_string()", setter(into))]
     metric_category: String,
 
