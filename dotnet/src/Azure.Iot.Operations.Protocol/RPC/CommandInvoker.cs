39e7f01800e2c33ed461aa0498f9379974423a66
Tim Taylor <timtay@microsoft.com>
2026-09-23 14:54:59 -0700

    fixup

--- dotnet/src/Azure.Iot.Operations.Protocol/RPC/CommandInvoker.cs
+++ dotnet/src/Azure.Iot.Operations.Protocol/RPC/CommandInvoker.cs
@@ -551,6 +551,14 @@ namespace Azure.Iot.Operations.Protocol.RPC
                     MessageExpiryInterval = (uint)reifiedCommandTimeout.TotalSeconds,
                 };
 
+                if (metadata != null && metadata.PersistCommand)
+                {
+                    // Only set this value if it is true since sending "aio-persistence":"false"
+                    // is the same as not sending "aio-persistence" user property at all
+                    requestMessage.AioPersistence = true;
+                    requestMessage.Retain = true;
+                }
+
                 string? clientId = _mqttClient.ClientId;
                 if (string.IsNullOrEmpty(clientId))
                 {
